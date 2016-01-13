3b88ce4c3bf58dd63f6a651aa82302da9712102f
Patrick Cunningham <pcunningham@Patricks-MacBook-Pro.local>
2016-01-13 15:17:21 -0500

    added report model

---
+++ database/migrations/2016_01_13_201024_create_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('subject');
+            $table->integer('opens');
+            $table->integer('clicks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reports');
+    }
+}
