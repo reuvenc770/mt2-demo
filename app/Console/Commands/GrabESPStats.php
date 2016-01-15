ec3b87447a39e07623ac46e06491cb1914689f69
Patrick Cunningham <pcunningham@Patricks-MacBook-Pro.local>
2016-01-15 10:09:51 -0500

    added console command

--- app/Console/Commands/GrabESPStats.php
+++ app/Console/Commands/GrabESPStats.php
@@ -2,16 +2,21 @@
 
 namespace App\Console\Commands;
 
+use App\Factories\APIFactory;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
-
+use App\Jobs\RetrieveReports;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 class GrabESPStats extends Command
 {
+    use DispatchesJobs;
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
     protected $signature = 'reports:downloadESP {espName}';
+    protected $factory;
 
     /**
      * The console command description.
@@ -20,10 +25,10 @@ class GrabESPStats extends Command
      */
     protected $description = 'Command description';
 
+
     /**
-     * Create a new command instance.
-     *
-     * @return void
+     * GrabESPStats constructor.
+     * @param APIFactory $factory
      */
     public function __construct()
     {
@@ -37,7 +42,11 @@ class GrabESPStats extends Command
      */
     public function handle()
     {
-        //Grab all the accounts for esp.  
+        $date = Carbon::now()->subDay(5)->toDateString();
+        $espName = $this->argument('espName');
+        //Grab all the accounts for esp.
         $this->info('CRONNNN');
+        //FOREACH ACCOUNT NUMBER FOR GIVEN ESP
+        $this->dispatch(new RetrieveReports($espName, "BH001", $date));
     }
 }
