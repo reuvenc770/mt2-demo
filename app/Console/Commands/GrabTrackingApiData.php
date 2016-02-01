97d8a62e12aee81a63cce4633b69b54596ae8e7b
vagrant <vagrant@homestead>
2016-02-01 23:12:28 +0000

    New command for pulling tracking data

---
+++ app/Console/Commands/GrabTrackingApiData.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+
+use App\Repositories\TrackingRepo;
+use Carbon\Carbon;
+use App\Jobs\RetrieveTrackingDataJob; 
+use Illuminate\Console\Command;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+
+class GrabTrackingApiData extends Command
+{
+    use DispatchesJobs;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reports:downloadTrackingData {trackingSource} {lookBack?}';
+    protected $trackingRepo;
+    protected $lookBack;
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->trackingSource = $this->argument('trackingSource');
+        $this->lookBack = $this->argument('lookBack') ? $this->argument('lookBack') : env('LOOKBACK',5);
+
+        $startDate = Carbon::now()->subDay($this->lookBack)->toDateString();
+        $endDate = Carbon::now()->toDateString();
+
+        $cakeLog = "Running Cake from {$startDate} to {$endDate}";
+        $this->info($cakeLog);
+        $this->dispatch(new RetrieveTrackingDataJob($this->trackingSource, $startDate, $endDate, str_random(16)));
+
+    }
+}
