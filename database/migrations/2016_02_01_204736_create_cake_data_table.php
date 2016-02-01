61782633b562aa936b1efa5ffad5b446511f7b1f
vagrant <vagrant@homestead>
2016-02-01 23:16:40 +0000

    Migration for cake_aggregate_data table

---
+++ database/migrations/2016_02_01_204736_create_cake_data_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCakeDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cake_aggregated_data', function (Blueprint $table) {
+            $table->increments('id');
+            $table->smallInteger('advertiser_id')->default(0);
+            $table->mediumInteger('affiliate_id')->default(0);
+            $table->mediumInteger('offer_id')->default(0);
+            $table->mediumInteger('creative_id')->default(0);
+            $table->string('subid_1', 100)->default('');
+            $table->string('subid_2', 100)->default('');
+            $table->string('subid_3', 100)->default('');
+            $table->string('subid_4', 100)->default('');
+            $table->string('subid_5', 100)->default('');
+            $table->date('date')->default('0000-00-00');
+            $table->integer('clicks')->default(0);
+            $table->integer('conversions')->default(0);
+            $table->decimal('revenue', 7, 2)->default(0.00);
+            $table->timestamps();
+
+            $table->unique(array(
+                'advertiser_id', 
+                'affiliate_id', 
+                'offer_id', 
+                'creative_id',
+                'subid_1',
+                'subid_2',
+                'subid_3',
+                'subid_4',
+                'subid_5',
+                'date'
+                ),
+                'adv_aff_off_cre_s1_s2_s3_s4_s5_d'
+            );
+            $table->index('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cake_aggregated_data');
+    }
+}
