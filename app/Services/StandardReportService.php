7dd9866941a3cf2d21fad753829f98ac1c4388e7
rbertorelli <rbertorelli@zetainteractive.com>
2016-02-04 21:35:27 +0000

    Moved query to repo (should never have been here to start)

--- app/Services/StandardReportService.php
+++ app/Services/StandardReportService.php
@@ -4,41 +4,16 @@ namespace App\Services;
 
 use App\Repositories\StandardReportRepo;
 
+
 class StandardReportService {
     protected $repo;
 
-    public function __construct(StandardReportRepo $reportRepo){
+    public function __construct($reportRepo){
        $this->repo = $reportRepo;
     }
 
     public function insertStandardStats($data){
-        //$this->repo->insertStats($standardReport);
-
-        // Custom query needed - 
-        // unfortunately, CAKE only has one item useful for matching:
-        // the s1 value, which is the first part of the deploy_id
-        // so we need to use LIKE
-
-        DB::statement("
-            UPDATE 
-                standard_reports
-            SET
-                t_creative_id = ?,
-                t_offer_id = ?,
-                t_clicks = ?,
-                conversions = ?,
-                revenue = ?
-            WHERE
-                deploy_id LIKE ?%
-        ",
-        [
-            $data['t_creative_id'],
-            $data['t_offer_id'],
-            $data['t_clicks'],
-            $data['conversions'],
-            $data['revenue'],
-            $data['subid_1'] . '_'
-        ]);
+        $this->repo->insertStats($data);
     }
 
 }
