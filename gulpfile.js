24f80c66982811e8df8faa3939c02fc6450f8adb
Adam Chin <achin@zetainteractive.com>
2016-02-09 18:00:59 +0000

    commiting current changes so I can rebase with Pats changes on master.

--- gulpfile.js
+++ gulpfile.js
@@ -18,14 +18,20 @@ elixir(function(mix) {
 
     mix.copy( 'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js' , 'public/js/bootstrap.min.js' );
 
+    mix.copy( 'node_modules/jasny-bootstrap/dist/js/jasny-bootstrap.min.js' , 'public/js/jasny-bootstrap.min.js' );
+
     mix.copy( 'node_modules/angular/angular.min.js' , 'public/js/angular.min.js' );
 
     mix.copy( 'node_modules/jquery/dist/jquery.min.js' , 'public/js/jquery.min.js' );
 
-    //ESP Angular Dependencies
     mix.scripts( [ 'app.js' , 'mt2app/GenericTableDirective.js' , 'mt2app/EditButtonDirective.js'] , 'public/js/angular_base.js' );
     mix.scripts( [  'esp/EspController.js' , 'esp/EspApiService.js' ] , 'public/js/esp.js' );
     mix.scripts( [  'user/UserController.js' , 'user/UserApiService.js' ] , 'public/js/user.js' );
     mix.scripts( [  'role/RoleController.js' , 'role/RoleApiService.js' ] , 'public/js/role.js' );
+
+    //old gulp tasks, will remove after making my changes
+    mix.scripts( [ 'app.js' , 'mt2app/GenericTableDirective.js' , 'mt2app/EditButtonDirective.js' , 'esp/EspController.js' , 'esp/EspApiService.js' ] , 'public/js/esp.js' );
+    mix.scripts( [ 'app.js' , 'mt2app/GenericTableDirective.js' , 'mt2app/EditButtonDirective.js' , 'client/ClientController.js' , 'client/ClientApiService.js' ] , 'public/js/client.js' );
+
     mix.copy( 'resources/assets/js/templates' , 'public/js/templates' );
 });
