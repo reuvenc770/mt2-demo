30ab04bb4a38f7a481e5c281881fb50263f5de79
Patrick Cunningham <pcunningham@Patricks-MacBook-Pro.local>
2016-02-17 14:45:16 -0500

    login/out and user bar

--- resources/views/layout/top-nav.blade.php
+++ resources/views/layout/top-nav.blade.php
@@ -5,5 +5,10 @@
     @foreach ( $menu->getMenu() as $current )
         <li ng-class="{ active : {{ $current[ 'active' ] }} }"><a href="{{ $current[ 'uri' ] }}" target="_self">{{ $current[ 'name' ] }}</a></li>
     @endforeach
+        @if(Sentinel::check())
+            <li><a href="{{route("logout")}}" target="_self">Log Out</a></li>
+        @else
+            <li><a href="{{route("login")}}" target="_self">Log In</a></li>
+        @endif
     </ul>
 </nav>
