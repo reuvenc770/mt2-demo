b7904cbb60a469bfcaf4798384a771d6cc57d84b
rbertorelli <rbertorelli@zetainteractive.com>
2016-03-02 17:52:32 +0000

    Blades and main form

---
+++ resources/views/pages/dataexport/dataexport-index.blade.php
@@ -0,0 +1,50 @@
+@extends('layout.default')
+
+@section('title', 'Data Exports')
+
+@section('content')
+<div class="row">
+  <div class="page-header col-xs-12"><h1 class="text-center">Data Exports</h1></div>
+</div>
+<div ng-controller="DataExportController as dataExport" ng-init="dataExport.loadActiveDataExports()">
+  @if (Sentinel::hasAccess('dataExport.add'))
+  <div class="row">
+    <button type="button" class="btn btn-info btn-lg pull-right mt2-header-btn" ng-click="dataExport.viewAdd()">
+      <span class="glyphicon glyphicon-plus"></span>
+      Add Client Group
+    </button>
+  </div>
+  @endif
+
+  <div class="row">
+    <div class="col-xs-12">
+        <div class="row">
+          <div class="col-xs-3 col-sm-2 col-md-2 col-lg-1">
+            <pagination-count recordcount="dataExport.paginationCount" currentpage="dataExport.currentPage"></pagination-count>
+          </div>
+
+          <div class="col-xs-9 col-sm-10 col-md-10 col-lg-11">
+            <pagination currentpage="dataExport.currentPage" maxpage="dataExport.pageCount"></pagination>
+          </div>
+        </div>
+
+        <dataexport-table records="dataExport.dataExports" loadingflag="clientGroup.currentlyLoading" >
+        </dataexport-table>
+
+        <div class="row">
+          <div class="col-xs-3 col-sm-2 col-md-2 col-lg-1">
+            <pagination-count recordcount="dataExport.paginationCount" currentpage="dataExport.currentPage"></pagination-count>
+          </div>
+
+          <div class="col-xs-9 col-sm-10 col-md-10 col-lg-11">
+            <pagination currentpage="dataExport.currentPage" maxpage="dataExport.pageCount"></pagination>
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
+@stop
+
+@section('pageIncludes')
+<script src="js/dataexport.js"></script>
+@stop
