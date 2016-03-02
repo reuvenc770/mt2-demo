20b6d3e04acf7e77c3aa5a206c1467441bab0cbe
rbertorelli <rbertorelli@zetainteractive.com>
2016-03-02 17:52:32 +0000

    Blades and main form

---
+++ resources/views/pages/dataexport/dataexport-add.blade.php
@@ -0,0 +1,35 @@
+@extends( 'layout.default' )
+
+@section( 'title' , 'Create Data Export' )
+
+@section( 'content' )
+<div class="row">
+    <div class="page-header col-xs-12"><h1 class="text-center">Create Data Export</h1></div>
+</div>
+
+<div ng-controller="DataExportController as dataExport" ng-init="dataExport.getDataExport()">
+    <div class="row">
+        <div class="hidden-xs hidden-sm col-md-3"></div>
+
+        <div class="col-xs-12 col-md-6">
+            <button type="button" class="btn btn-success btn-md pull-right" ng-class="{ 'disabled' : dataExport.creatingDataExport }" ng-click="dataExport.saveDataExport( $event )">
+                <span class="glyphicon glyphicon-save" ng-class="{ 'rotateMe' : dataExport.creatingDataExport }"></span>
+                Save
+            </button>
+
+            <div class="clearfix"></div>
+
+            @include( 'pages.dataexport.dataexport-form' )
+
+            <button type="button" class="btn btn-success btn-md pull-right" ng-class="{ 'disabled' : dataExport.creatingDataExport }" ng-click="dataExport.saveDataExport( $event )">
+                <span class="glyphicon glyphicon-save" ng-class="{ 'rotateMe' : dataExport.creatingDataExport }"></span>
+                Save
+            </button>
+        </div>
+    </div>
+</div>
+@stop
+
+@section( 'pageIncludes' )
+<script src="js/dataexport.js"></script>
+@stop
