87537f9d66c377b718bad8c149deb249b2ee6fd2
rbertorelli <rbertorelli@zetainteractive.com>
2016-03-08 23:16:33 +0000

    Models with eloquent relationships

---
+++ app/Models/Email.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Email extends Model {
+    
+  public function emailClientInstances() {
+    return $this->hasMany('App\Model\EmailClientInstance');
+  }
+
+  public function emailDomain() {
+    return $this->belongsTo('App\Model\EmailDomain');
+  }
+
+  public function emailAction() {
+    return $this->hasMany('App\Model\EmailAction');
+  }
+
+  public function emailCampaignStatistic() {
+    return $this->hasMany('App\Model\emailCampaignStatistic');
+  }
+
+}
---
+++ app/Models/EmailDomain.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmailDomain extends Model {
+    
+  public function email() {
+    return $this->hasMany('App\Model\Email');
+  }
+
+  public function domainGroup() {
+    return $this->belongsTo('App\Model\DomainGroup');
+  }
+}
