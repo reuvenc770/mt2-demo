865589d9c38ea32ca4feac70dc4b8a43eb6c41a2
rbertorelli <rbertorelli@zetainteractive.com>
2016-03-17 20:17:42 +0000

    Updates to email_campaign_statistics table (WIP)

--- app/Repositories/EmailCampaignStatisticRepo.php
+++ app/Repositories/EmailCampaignStatisticRepo.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Repositories;
-
 use App\Models\EmailCampaignStatistic;
+use DB;
 
 class EmailCampaignStatistic {
     protected $model;
@@ -18,6 +18,70 @@ class EmailCampaignStatistic {
                 a. If it does not, insert and set appropriate rows
                 b. else: update appropriate rows
         */
+        switch ($actionType) {
+            // NOW, ARE LISTS DUPLICATES?
+            case "opener":
+                DB::connection('reporting_data')->statement(
+                    "INSERT INTO email_campaign_statistic 
+                    (email_id, campaign_id, last_status, esp_first_open_datetime,
+                    esp_last_open_datetime, esp_total_opens, created_at,
+                    updated_at) VALUES (:email_id, :campaign_id, 'opener', :dt,
+                    :dt, 1, NOW(), NOW()) 
+
+                    ON DUPLICATE KEY UPDATE
+                        email_id=email_id,
+                        campaign_id=campaign_id,
+                        last_status = 'opener',
+                        esp_first_open_datetime=esp_first_open_datetime,
+                        esp_last_open_datetime = :dt,
+                        esp_total_opens = esp_total_opens + 1,
+                        created_at=created_at,
+                        updated_at = NOW()",
+                    array(
+                        'email_id' => $row['email_id'],
+                        'campaign_id' => $row['campaign_id'],
+                        'dt' => $row['datetime']
+                    )
+                );
+                break;
+
+            case "clicker":
+
+                /*
+                    Danny Espaillat: "We should treat the opener and
+                    clicker files as exclusing [sic]"
+                
+                    Thus, for clickers, set 
+                */
+
+                DB::connection('reporting_data')->statement(
+                    "INSERT INTO email_campaign_statistic 
+                    (email_id, campaign_id, last_status, esp_first_open_datetime,
+                    esp_last_open_datetime, esp_total_opens, created_at,
+                    updated_at) VALUES (:email_id, :campaign_id, 'clicker', :dt,
+                    :dt, 1, NOW(), NOW()) 
+
+                    ON DUPLICATE KEY UPDATE
+                        email_id=email_id,
+                        campaign_id=campaign_id,
+                        last_status = 'clicker',
+                        esp_first_open_datetime=esp_first_open_datetime,
+                        esp_last_open_datetime = :dt,
+                        esp_total_opens = esp_total_opens + 1,
+                        created_at=created_at,
+                        updated_at = NOW()",
+                    array(
+                        'email_id' => $row['email_id'],
+                        'campaign_id' => $row['campaign_id'],
+                        'dt' => $row['datetime']
+                    )
+                );
+                break;
+
+            case
+            default:
+                break;
+        }
     }
 
 
