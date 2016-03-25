975d56c14d0cd1ea767fcc7305e5e494200b25a9
rbertorelli <rbertorelli@zetainteractive.com>
2016-03-25 19:38:34 +0000

    Switching from array to object notation

--- app/Services/ImportMt1EmailsService.php
+++ app/Services/ImportMt1EmailsService.php
@@ -26,34 +26,34 @@ class ImportMt1EmailsService
 
         foreach ($records as $id => $record) {
             $record = $this->mapToTempTable($record);
-            $this->$tempEmailRepo->insert($record);
+            $this->tempEmailRepo->insert($record);
         }
 
     }
 
     private function mapToTempTable($row) {
         return [
-            'email_id' => $row['email_user_id'],
-            'client_id' => $row['client_id'],
-            'email_addr' => $row['email_addr'],
-            'status' => $row['status'],
-            'first_name' => $row['first_name'],
-            'last_name' => $row['last_name'],
-            'address' => $row['address'],
-            'address2' => $row['address2'],
-            'city' => $row['city'],
-            'state' => $row['state'],
-            'zip' => $row['zip'],
-            'country' => $row['country'],
-            'dob' => $row['dob'],
-            'gender' => $row['gender'],
-            'phone' => $row['phone'],
-            'mobile_phone' => $row['mobile_phone'],
-            'work_phone' => $row['work_phone'],
-            'capture_date' => $row['capture_date'],
-            'ip' => $row['ip'],
-            'source_url' => $row['source_url'],
-            'last_updated' => $row['lastUpdated']
+            'email_id' => $row->email_user_id,
+            'client_id' => $row->client_id,
+            'email_addr' => $row->email_addr,
+            'status' => $row->status,
+            'first_name' => $row->first_name,
+            'last_name' => $row->last_name,
+            'address' => $row->address,
+            'address2' => $row->address2,
+            'city' => $row->city,
+            'state' => $row->state,
+            'zip' => $row->zip,
+            'country' => $row->country,
+            'dob' => $row->dob,
+            'gender' => $row->gender,
+            'phone' => $row->phone,
+            'mobile_phone' => $row->mobile_phone,
+            'work_phone' => $row->work_phone,
+            'capture_date' => $row->capture_date,
+            'ip' => $row->ip,
+            'source_url' => $row->source_url,
+            'last_updated' => $row->lastUpdated
         ];
     }
 
