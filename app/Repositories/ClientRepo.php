38033e4621983425054da302707b0601b7524d4c
rbertorelli <rbertorelli@zetainteractive.com>
2016-03-29 14:42:55 +0000

    Repositories for Client, EmailClientInstance, and EmailDomain

---
+++ app/Repositories/ClientRepo.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Client;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Query\Builder;
+
+/**
+ *
+ */
+class ClientRepo {
+  
+    private $client;
+
+    public function __construct(Client $client) {
+        $this->client = $client;
+    }
+
+    public function isActive($id) {
+        return $this
+                ->client
+                ->select('status')
+                ->where('id', $id)
+                ->get()[0]['status'] === 'Active';
+    }
+
+}
---
+++ app/Repositories/EmailClientInstanceRepo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\EmailClientInstance;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Query\Builder;
+
+/**
+ *
+ */
+class EmailClientInstanceRepo {
+
+    private $emailClientModel;
+
+    public function __construct(EmailClientInstance $emailClientModel) {
+        $this->emailClientModel = $emailClientModel;
+    }
+
+    public function getEmailId($emailAddress) {
+        #return $this->emailModel->select( 'id' )->where( 'email_address' , $email )->get();
+        return mt_rand(1, 100000);
+    }
+
+    public function insert($emailClientData) {
+        $this->emailClientModel->insert($emailClientData);
+    }
+
+}
