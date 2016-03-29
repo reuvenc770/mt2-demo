aec94a32b580e7966323e8d7be5bfbe558c45199
rbertorelli <rbertorelli@zetainteractive.com>
2016-03-29 14:44:59 +0000

    Fleshing out all methods and helpers needed for import service

--- app/Services/ImportMt1EmailsService.php
+++ app/Services/ImportMt1EmailsService.php
@@ -3,16 +3,35 @@
 namespace App\Services;
 use App\Repositories\TempStoredEmailRepo;
 use App\Services\API\Mt1DbApi;
+use App\Repositories\EmailRepo;
+use App\Repositories\EmailClientInstanceRepo;
+use App\Repositories\ClientRepo;
+use App\Repositories\EmailDomainRepo;
 
 class ImportMt1EmailsService
 {
 
     private $tempEmailRepo;
     private $api;
+    private $emailRepo;
+    private $emailClientRepo;
+    private $clientRepo;
+    private $emailDomainRepo;
 
-    public function __construct(Mt1DbApi $api, TempStoredEmailRepo $tempEmailRepo) {
+    public function __construct(
+        Mt1DbApi $api, 
+        TempStoredEmailRepo $tempEmailRepo, 
+        EmailRepo $emailRepo, 
+        EmailClientInstanceRepo $emailClientRepo,
+        ClientRepo $clientRepo,
+        EmailDomainRepo $emailDomainRepo) {
+
         $this->api = $api;
         $this->tempEmailRepo = $tempEmailRepo;
+        $this->emailRepo = $emailRepo;
+        $this->emailClientRepo = $emailClientRepo;
+        $this->clientRepo = $clientRepo;
+        $this->emailDomainRepo = $emailDomainRepo;
     }
 
     public function run() {
@@ -27,7 +46,22 @@ class ImportMt1EmailsService
         foreach ($records as $id => $record) {
             $record = $this->mapToTempTable($record);
             $this->tempEmailRepo->insert($record);
+
+            // insert into emails
+            // insert into email_client_instances
+            $clientId = $record['client_id'];
+
+            if ($this->clientRepo->isActive($clientId)) {
+                $emailRow = $this->mapToEmailTable($record);
+                $this->emailRepo->insertCopy($emailRow);
+
+                $emailClientRow = $this->mapToEmailClientTable($record);
+                $this->emailClientRepo->insert($emailClientRow);
+            }
+
         }
+        // Delete records
+        $this->api->cleanTable();
 
     }
 
@@ -57,4 +91,42 @@ class ImportMt1EmailsService
         ];
     }
 
+    private function mapToEmailTable($row) {
+        return [
+            'id' => $row['email_id'],
+            'email_address' => $row['email_addr'],
+            'email_domain_id' => $this->emailDomainRepo->getIdForName($row['email_addr'])
+        ];
+    }
+
+    private function mapToEmailClientTable($row) {
+        return [
+            'email_id' => $row['email_id'],
+            'client_id' => $row['client_id'],
+            'subscribe_datetime' => 'NOW()', 
+            'unsubscribe_datetime' => null, // null for now, at least
+            'status' => $this->convertStatus($row['status']),
+            'first_name' => $row['first_name'],
+            'last_name' => $row['last_name'],
+            'address' => $row['address'],
+            'address2' => $row['address2'],
+            'city' => $row['city'],
+            'state' => $row['state'],
+            'zip' => $row['zip'],
+            'country' => $row['country'],
+            'dob' => $row['dob'],
+            'gender' => $row['gender'],
+            'phone' => $row['phone'],
+            'mobile_phone' => $row['mobile_phone'],
+            'work_phone' => $row['work_phone'],
+            'capture_date' => $row['capture_date'],
+            'source_url' => $row['source_url'],
+            'ip' => $row['ip']
+        ];
+
+    }
+
+    private function convertStatus($status) {
+        return $status === 'Active' ? 'A' : 'U';
+    }
 }
