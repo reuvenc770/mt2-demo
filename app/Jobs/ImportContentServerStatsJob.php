52c2616932690ca2c34ae1f988b644e390d46d00
rbertorelli <rbertorelli@zetainteractive.com>
2016-04-01 22:23:22 +0000

    New migration, command, job, model, repo, and service for pulling content server stats

---
+++ app/Jobs/ImportContentServerStatsJob.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Facades\JobTracking;
+use App\Factories\APIFactory;
+use App\Models\JobEntry;
+
+class ImportContentServerStatsJob extends Job implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+    const JOB_NAME = "DownloadContentServerStats";
+    private $tracking;
+    private $maxAttempts;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($tracking) {
+        $this->tracking = $tracking;
+        $this->maxAttempts = env('MAX_ATTEMPTS', 3);
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle() {
+        JobTracking::startAggregationJob(self::JOB_NAME, $this->tracking);
+
+        if ($this->attempts() > $this->maxAttempts) {
+            $this->release(1);
+        }
+
+        $service = APIFactory::createMt1DataImportService(self::JOB_NAME);
+        $service->run();
+
+        JobTracking::changeJobState(JobEntry::SUCCESS,$this->tracking, $this->attempts());
+    }
+
+    public function failed() {
+        JobTracking::changeJobState(JobEntry::FAILED,$this->tracking, $this->maxAttempts);
+    }
+}
---
+++ app/Repositories/ContentServerActionRepo.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\ContentServerAction;
+use DB;
+use Illuminate\Database\Query\Builder;
+
+/**
+ *
+ */
+class ContentServerActionRepo {
+
+    private $model;
+
+    public function __construct(ContentServerAction $model) {
+        $this->model = $model;
+    }
+
+    public function insert($data) {
+        $this->model->insert($data);
+    }
+
+    public function loadInfile($name) {
+        $path = storage_path() . '/' . $name;
+        DB::unprepared("
+            LOAD DATA LOCAL INFILE '$path' IGNORE INTO TABLE
+            content_server_actions FIELDS TERMINATED BY ','
+            OPTIONALLY ENCLOSED BY '`' LINES TERMINATED BY '\\n'
+            (email_id, sub_id, action_type, send_date, action_time)
+        ");
+    }
+
+    public function pullAggregatedStats() {
+        return $this->model
+            ->select(DB::raw('email_id, 
+                sub_id, 
+                SUM(IF(action_type = 'opener', 1, 0)) AS opens, 
+                MIN(CASE WHEN action_type = 'opener' THEN action_time END) AS min_open_date, 
+                MAX(CASE WHEN action_type = 'opener' THEN action_time END) AS max_open_date, 
+                SUM(IF(action_type = 'clicker', 1, 0)) AS clicks, 
+                MIN(CASE WHEN action_type = 'clicker' THEN action_time END) AS min_click_date, 
+                MAX(CASE WHEN action_type = 'clicker' THEN action_time END) AS max_click_date'))
+            ->where('action_time', ['CURDATE() - INTERVAL 1 DAY', 'CURDATE()'])
+            ->groupBy('email_id', 'sub_id')
+            ->get();
+    }
+
+}
