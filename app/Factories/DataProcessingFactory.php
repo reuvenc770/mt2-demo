07b28197e7a8dc0388943809e4a58ce09b775eca
rbertorelli <rbertorelli@zetainteractive.com>
2016-04-07 15:18:31 +0000

    Small refactor of DataProcessingFactory

--- app/Factories/DataProcessingFactory.php
+++ app/Factories/DataProcessingFactory.php
@@ -28,38 +28,66 @@ class DataProcessingFactory {
     public static function create($name, $lookback) {
         switch($name) {
             case 'PopulateEmailCampaignStats':
-                $actionTypeModel = new ActionType();
-                $actionTypeRepo = new ActionRepo($actionTypeModel);
-                $actionModel = new EmailAction();
-                $actionsRepo = new EmailActionsRepo($actionModel);
-                $statsModel = new EmailCampaignStatistic();
-                $statsRepo = new EmailCampaignStatisticRepo($statsModel);
-                $actionMap = $actionTypeRepo->getMap();
-
-                return new EmailCampaignAggregationService($statsRepo, $actionsRepo, $actionMap, $lookback);
+                return $this->createEmailCampaignAggregationService($lookback);
 
             case('PullCakeDeliverableStats'):
-                $statsModel = new EmailCampaignStatistic();
-                $statsRepo = new EmailCampaignStatisticRepo($statsModel);
-
-                $trackingModel = new CakeData();
-                $trackingRepo = new TrackingRepo($trackingModel);
-
-                return new TrackingDeliverableService($statsRepo, $trackingRepo);
+                return $this->createTrackingDeliverableService();
 
             case('UpdateContentServerStats'):
+                return $this->createUpdateContentServerStatsService();
 
-                // need repo for content_server_actions
-                $contentActions = new ContentServerAction();
-                $contentActionsRepo = new ContentServerActionRepo($contentActions);
-
-                // need repo for email_campaign_statistics
-                $statsModel = new EmailCampaignStatistic();
-                $statsRepo = new EmailCampaignStatisticRepo($statsModel);
-                return new UpdateContentServerStatsService($contentActionsRepo, $statsRepo);
-
+            case('ProcessUserAgentsJob'):
+                return $this->createUserAgentProcessingService($lookback);
+                
             default:
                 throw new \Exception("Data processing service {$name} does not exist");
         }
     }
+
+    private static function createEmailCampaignAggregationService($lookback) {
+        $actionTypeModel = new ActionType();
+        $actionTypeRepo = new ActionRepo($actionTypeModel);
+        $actionModel = new EmailAction();
+        $actionsRepo = new EmailActionsRepo($actionModel);
+        $statsModel = new EmailCampaignStatistic();
+        $statsRepo = new EmailCampaignStatisticRepo($statsModel);
+        $actionMap = $actionTypeRepo->getMap();
+
+        return new EmailCampaignAggregationService($statsRepo, $actionsRepo, $actionMap, $lookback);
+    }
+
+    private static function createTrackingDeliverableService() {
+        $statsModel = new EmailCampaignStatistic();
+        $statsRepo = new EmailCampaignStatisticRepo($statsModel);
+
+        $trackingModel = new CakeData();
+        $trackingRepo = new TrackingRepo($trackingModel);
+
+        return new TrackingDeliverableService($statsRepo, $trackingRepo);        
+    }
+
+    private static function createUpdateContentServerStatsService() {
+        // need repo for content_server_actions
+        $contentActions = new ContentServerAction();
+        $contentActionsRepo = new ContentServerActionRepo($contentActions);
+
+        // need repo for email_campaign_statistics
+        $statsModel = new EmailCampaignStatistic();
+        $statsRepo = new EmailCampaignStatisticRepo($statsModel);
+
+        return new UpdateContentServerStatsService($contentActionsRepo, $statsRepo);      
+    }
+
+    private static function createUserAgentProcessingService($lookback) {
+        // feed off a source of new user agents
+        $sourceModel = new CakeData();
+        $sourceRepo = new TrackingRepo($trackingModel);
+
+        $userAgent = new \App\Models\UserAgentString();
+        $userAgentRepo = new \App\Repositories\UserAgentStringRepo($userAgent);
+
+        return new \App\Services\UserAgentProcessingService($sourceRepo, $userAgentRepo);
+    }
+
+
 }
