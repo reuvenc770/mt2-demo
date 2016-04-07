6b978e4464f9d12c836375532ef9bf8444cd4917
rbertorelli <rbertorelli@zetainteractive.com>
2016-04-07 14:39:52 +0000

    EmailActionsRepo.php edited online with Bitbucket - one final attempt at unbuffered queries for email_actions

--- app/Repositories/EmailActionsRepo.php
+++ app/Repositories/EmailActionsRepo.php
@@ -27,7 +27,7 @@ class EmailActionsRepo {
         // In line with expectations, this got enormous
         // running an unbuffered query (and returning the resource)
         $pdo = DB::connection('reporting_data')->getPdo();
-
+        /*
         $statement = $pdo->prepare(
             "SELECT * FROM email_actions WHERE id > :id", 
             array(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => false )
@@ -35,5 +35,13 @@ class EmailActionsRepo {
         $statement->bindParam(':id', $lookback, PDO::PARAM_INT);
         $statement->execute();
         return $statement;
+        */
+        
+        // Testing a slightly different implementation
+        // in the perhaps naive hope that it works
+        $pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+        $result = $pdo->query("SELECT * FROM email_actions WHERE id > $lookback");
+        return $result;
+        
     }
 }
