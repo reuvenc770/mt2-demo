fa899cee5c8973982c8ae002f576c39fbe7b12c2
Patrick Cunningham <pcunningham@Patricks-MacBook-Pro.local>
2016-04-07 17:16:22 -0400

    debugging

--- public/bitbuckettrigger.php
+++ public/bitbuckettrigger.php
@@ -1,5 +1,7 @@
 <?php
     // Do a git checkout to the web root
-    exec('~/.composer/vendor/bin/envoy run deploy --branch=master');
+    $test = null;
+    exec('~/.composer/vendor/bin/envoy run deploy --branch=master',$test);
     file_put_contents('storage/logs/deploy.log', date('m/d/Y h:i:s a') . " Deployed branch: " .  "master" . "\n", FILE_APPEND);
 
+print_r($test);
