183d28917ebaa8acc4a3a9aeb33fb15900669cb9
Adam Chin <achin@zetainteractive.com>
2016-04-19 20:52:27 +0000

    Have Raw and Standard stats integrated.

--- app/Services/PublicatorsReportService.php
+++ app/Services/PublicatorsReportService.php
@@ -59,72 +59,33 @@ class PublicatorsReportService extends AbstractReportService implements IDataSer
             $this->insertStats( $this->api->getEspAccountId() , $campaignData );
         }
 
-        #Event::fire( new RawReportDataWasInserted( $this , $data ) );
+        Event::fire( new RawReportDataWasInserted( $this , $data ) );
     }
 
     public function mapToRawReport ( $data ) {}
 
     public function mapToStandardReport ( $data ) {
+        $deployId = $this->parseSubID( $data[ "ListName"] );
+
         return [
-            'campaign_name' => '' , #need to go back and see if this is avail
-            'external_deploy_id' => '', #need to go back and see if sub id is avail
-            'm_deploy_id' => '', #need to go back and see if sub id is avail
-            'esp_account_id' => $report['esp_account_id'],
-            'esp_internal_id' => $report['internal_id'],
-        ];
-
-        /*
-        | external_deploy_id | int(11)          | YES  |     | NULL    |                |
-            | campaign_name      | varchar(255)     | NO   | UNI |         |                |
-            | m_deploy_id        | int(10) unsigned | NO   |     | 0       |                |
-            | esp_account_id     | int(10) unsigned | NO   |     | 0       |                |
-            | esp_internal_id    | int(11)          | NO   |     | 0       |                |
-            | datetime           | datetime         | NO   |     | NULL    |                |
-            | m_creative_id      | int(10) unsigned | NO   |     | 0       |                |
-            | m_offer_id         | int(10) unsigned | NO   |     | 0       |                |
-            | name               | varchar(255)     | YES  |     | NULL    |                |
-            | subject            | varchar(255)     | YES  |     | NULL    |                |
-            | from               | varchar(255)     | YES  |     | NULL    |                |
-            | from_email         | varchar(255)     | YES  |     | NULL    |                |
-            | m_sent             | int(10) unsigned | YES  |     | NULL    |                |
-            | e_sent             | int(10) unsigned | YES  |     | NULL    |                |
-            | delivered          | int(10) unsigned | YES  |     | NULL    |                |
-            | bounced            | int(10) unsigned | YES  |     | NULL    |                |
-            | optouts            | int(10) unsigned | YES  |     | NULL    |                |
-            | m_opens            | int(10) unsigned | YES  |     | NULL    |                |
-            | e_opens            | int(10) unsigned | YES  |     | NULL    |                |
-            | t_opens            | int(10) unsigned | YES  |     | NULL    |                |
-            | m_opens_unique     | int(10) unsigned | YES  |     | NULL    |                |
-            | e_opens_unique     | int(10) unsigned | YES  |     | NULL    |                |
-            | t_opens_unique     | int(10) unsigned | YES  |     | NULL    |                |
-            | m_clicks           | int(10) unsigned | YES  |     | NULL    |                |
-            | e_clicks           | int(10) unsigned | YES  |     | NULL    |                |
-            | t_clicks           | int(10) unsigned | YES  |     | NULL    |                |
-            | m_clicks_unique    | int(10) unsigned | YES  |     | NULL    |                |
-            | e_clicks_unique    | int(10) unsigned | YES  |     | NULL    |                |
-            | t_clicks_unique    | int(10) unsigned | YES  |     | NULL    |                |
-            | conversions        | int(11)          | YES  |     | NULL    |                |
-            | cost               | decimal(7,2)     | YES  |     | NULL    |                |
-            | revenue            | decimal(7,2)     | YES  |     | NULL    |                |
-
-           return array(
-            'campaign_name' => $report['name'],
-            'external_deploy_id' => $deployId,
-            'm_deploy_id' => $deployId,
-            'esp_account_id' => $report['esp_account_id'],
-            'esp_internal_id' => $report['internal_id'],
-            'datetime' => '0000-00-00', //$report[''],
-            'name' => $report['name'],
-            'subject' => $report['subject'],
-            'from' => $report['from_name'],
-            'from_email' => $report['from_email'],
-            'e_sent' => $report['sent'],
-            'delivered' => $report['delivered'],
-            'bounced' => (int)$report['hard_bounces'],
-            'optouts' => $report['unsubs'],
-            'e_opens' => $report['opens'],
-            'e_clicks' => $report['clicks']
-        ); 
-         */
+            "external_deploy_id" => $deployId , 
+            "campaign_name" => $data[ "ListName" ] ,
+            "m_deploy_id" => $deployId ,
+            "esp_account_id" => $data[ "esp_account_id" ] ,
+            "esp_internal_id" => $data[ "internal_id" ] ,
+            "datetime" => $data[ "SentDate" ] ,
+            "name" => $data[ "ListName" ] ,
+            "subject" => $data[ "Subject" ] ,
+            "from" => $data[ "FromName" ] ,
+            "from_email" => $data[ "FromEmail" ] ,
+            "e_sent" => $data[ "TotalMailsSent" ] ,
+            "delivered" => $data[ "TotalMailsSent" ] - $data[ "TotalBounces" ] ,
+            "bounced" => $data[ "TotalBounces" ] ,
+            "optouts" => $data[ "TotalUniqueUnsubscribed" ] ,
+            "e_opens" => $data[ "TotalOpened" ] ,
+            "e_opens_unique" => $data[ "TotalUniqueOpened" ] ,
+            "e_clicks" => $data[ "TotalClicks" ] ,
+            "e_clicks_unique" => $data[ "TotalUniqueClicks" ]
+        ]; 
     }
 }
