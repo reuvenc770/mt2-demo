f20226129dd1f35b01962d2a5e6a9abbb175e9a0
Patrick Cunningham <pcunningham@Patricks-MacBook-Pro.local>
2016-05-12 12:29:30 -0400

    more power

--- app/Library/Campaigner/ContactManagement.php
+++ app/Library/Campaigner/ContactManagement.php
@@ -104,8 +104,8 @@ class ContactManagement extends \SoapClient
             'compression'=> SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP,
             'soap_version'=> 'SOAP_1_1',
             'trace' => true,
-            'connection_timeout' => 600,
-            'default_socket_timeout' => 600
+            'connection_timeout' => 1000,
+            'default_socket_timeout' => 1000
         ), $options);
         try {
             parent::__construct($wsdl, $options);
