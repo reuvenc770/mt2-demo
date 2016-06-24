dc0925bf77fa81c964dbde3178fe260bab0ef60e
rbertorelli <rbertorelli@zetainteractive.com>
2016-06-24 20:21:37 +0000

    DeploysMissingDataFound.php edited online with Bitbucket - trait got into event by mistake

--- app/Events/DeploysMissingDataFound.php
+++ app/Events/DeploysMissingDataFound.php
@@ -5,11 +5,10 @@ namespace App\Events;
 use App\Events\Event;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class DeploysMissingDataFound extends Event
 {
-    use SerializesModels, ShouldQueue;
+    use SerializesModels;
     private $specifiedDeploys;
 
     /**
