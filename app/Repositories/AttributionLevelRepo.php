d91d7454dc2b1e32d2b7160abcf590baa151fa4e
Adam Chin <achin@zetainteractive.com>
2016-06-29 21:31:51 +0000

    Adding temp table generation, deletion, and model overriding.

--- app/Repositories/AttributionLevelRepo.php
+++ app/Repositories/AttributionLevelRepo.php
@@ -6,12 +6,19 @@
 namespace App\Repositories;
 
 use App\Models\AttributionLevel;
+use Illuminate\Database\Schema\Blueprint;
 
 class AttributionLevelRepo {
+    const BASE_TABLE_NAME = 'attribution_levels_model';
+
     protected $levels;
 
-    public function __construct ( AttributionLevel $levels ) {
+    public function __construct ( AttributionLevel $levels , $attributionModelId = null ) {
         $this->levels = $levels;
+
+        if ( !is_null( $attributionModelId ) && is_numeric( $attributionModelId ) ) {
+            $this->table = self::BASE_TABLE_NAME . $attributionModelId;
+        }
     }
 
     public function setLevel ( $clientId , $level ) {
@@ -28,5 +35,21 @@ class AttributionLevelRepo {
 
     public function toggleActiveStatus ( $clientId , $isActive ) {
         #sets active filed for the given client.
+    }
+
+    public function generateTempTable ( $modelId ) {
+        Schema::create( self::BASE_TABLE_NAME . $modelId , function (Blueprint $table) {
+            $table->integer( 'client_id' )->unsigned();
+            $table->integer( 'level' )->unsigned();
+            $table->boolean( 'active' )->default( true );
+            $table->timestamps();
+
+            $table->primary( 'client_id' );
+            $table->index( [ 'client_id' , 'level' ] );
+        });
+    }
+
+    public function dropTempTable ( $modelId ) {
+        Schema::drop( self::BASE_TABLE_NAME . $modelId );
     }
 }
