73eb9ae60da8d7f53be24cc1f07f3fe1bb0a76da
rbertorelli <rbertorelli@zetainteractive.com>
2016-06-30 16:37:41 +0000

    Removed debug for reruns

--- app/Console/Commands/GrabDeliverableReports.php
+++ app/Console/Commands/GrabDeliverableReports.php
@@ -60,8 +60,6 @@ class GrabDeliverableReports extends Command
         if ( preg_match( '/:/' , $espName ) ) {
             $espParts = explode( ':' , $espName );
             $espName = $espParts[ 0 ];
-            echo "Executing rerun for $espName" . PHP_EOL;
-
             $processState = [ 'pipe' => $espParts[ 1 ] ];
         }
 
