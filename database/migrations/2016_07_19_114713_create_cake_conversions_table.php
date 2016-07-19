16ea135073a80246928fc51d94ca0fa72c358df4
Adam Chin <achin@zetainteractive.com>
2016-07-19 21:25:21 +0000

    Changed columns to be snakecase, added s3 in case we need it, and email_id for storing after parsing the subid.

--- database/migrations/2016_07_19_114713_create_cake_conversions_table.php
+++ database/migrations/2016_07_19_114713_create_cake_conversions_table.php
@@ -17,31 +17,33 @@ class CreateCakeConversionsTable extends Migration
     {
         Schema::connection( 'reporting_data' )->create('cake_conversions', function (Blueprint $table) {
             $table->increments('id');
+            $table->string( 'email_id' )->default( 0 );
             $table->string( 's1' )->default( '' );
             $table->string( 's2' )->default( '' );
+            $table->string( 's3' )->default( '' );
             $table->string( 's4' )->default( '' );
             $table->string( 's5' )->default( '' );
-            $table->dateTime( 'clickDate' );
-            $table->dateTime( 'campaignDate' );
-            $table->integer( 'clickId' )->unsigned();
-            $table->dateTime( 'conversionDate' )->nullable();
-            $table->integer( 'conversionId' )->default( 0 );
-            $table->integer( 'requestSessionId' );
-            $table->integer( 'affiliateId' );
-            $table->integer( 'offerId' );
-            $table->integer( 'advertiserId' );
-            $table->integer( 'campaignId' );
-            $table->integer( 'creativeId' );
-            $table->text( 'userAgentString' )->default( '' );
-            $table->decimal( 'priceReceived' , 7 , 4 )->default( 0.0000 );
-            $table->decimal( 'pricePaid' , 7 , 4 )->default( 0.0000 );
-            $table->tinyInteger( 'pricePaidCurrencyId' )->default( 1 );
-            $table->tinyInteger( 'priceReceivedCurrencyId' )->default( 1 );
+            $table->dateTime( 'click_date' );
+            $table->dateTime( 'campaign_date' );
+            $table->integer( 'click_id' )->unsigned();
+            $table->dateTime( 'conversion_date' )->nullable();
+            $table->integer( 'conversion_id' )->default( 0 );
+            $table->integer( 'request_session_id' );
+            $table->integer( 'affiliate_id' );
+            $table->integer( 'offer_id' );
+            $table->integer( 'advertiser_id' );
+            $table->integer( 'campaign_id' );
+            $table->integer( 'creative_id' );
+            $table->text( 'user_agent_string' )->default( '' );
+            $table->decimal( 'price_received' , 7 , 4 )->default( 0.0000 );
+            $table->decimal( 'price_paid' , 7 , 4 )->default( 0.0000 );
+            $table->tinyInteger( 'price_paid_currency_id' )->default( 1 );
+            $table->tinyInteger( 'price_received_currency_id' )->default( 1 );
             $table->string( 'ip' )->nullable();
             $table->timestamps();
 
             $table->index( 's1' );
-            $table->index( 'campaignDate' );
+            $table->index( 'campaign_date' );
         });
     }
 
