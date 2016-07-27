76464c129afde814e479c3afdc1240ea1c4e5f2b
rbertorelli <rbertorelli@zetainteractive.com>
2016-07-27 20:40:01 +0000

    Added command and kernel entry. New method in DataProcessingFactory. Made service conform to DataProcessing job. Added conditional check in service for missing deploys

--- app/Services/PopulateCfsStatsService.php
+++ app/Services/PopulateCfsStatsService.php
@@ -7,6 +7,7 @@ use App\Repositories\StandardApiReportRepo;
 use App\Repositories\CreativeClickthroughRateRepo;
 use App\Repositories\FromOpenRateRepo;
 use App\Repositories\SubjectOpenRateRepo;
+use Carbon\Carbon;
 
 class PopulateCfsStatsService
 {
@@ -16,6 +17,7 @@ class PopulateCfsStatsService
     private $fromRepo;
     private $creativeRepo;
     private $reportRepo;
+    private $mt1DeployData;
 
     public function __construct(EspAdvertiserJoinRepo $mt1Repo,
                                 StandardApiReportRepo $reportRepo,
@@ -30,27 +32,31 @@ class PopulateCfsStatsService
         $this->reportRepo = $reportRepo;
     }
 
-    public function run($date) {
+    public function extract($lookback) {
+        $date = Carbon::today()->subDays($lookback)->format('Y-m-d');
+        $this->mt1DeployData = $this->mt1Repo->getUpdatedFrom($date);
+    }
 
-        $mt1DeployData = $this->mt1Repo->getUpdatedFrom($date);
-
-        foreach ($mt1DeployData as $deploy) {
+    public function load() {
+        foreach ($this->mt1DeployData as $deploy) {
             $deployId = $deploy->deploy_id;
             $creativeId = $deploy->creative_id;
             $subjectId = $deploy->subject_id;
             $fromId = $deploy->from_id;
 
             $stats = $this->reportRepo->getStatsForDeploy($deployId);
-            $delivers = (int)$stats->delivers;
-            $opens = (int)$stats->opens;
-            $clicks = (int)$stats->clicks;
+            if ($stats) {
+                $delivers = (int)$stats->delivers;
+                $opens = (int)$stats->opens;
+                $clicks = (int)$stats->clicks;
 
-            // Currently list profile id is 0 because MT1 does not save that information
-            $listProfileId = 0;
-            $this->creativeRepo->saveStats($creativeId, $listProfileId, $deployId, $opens, $clicks);
-            $this->subjRepo->saveStats($subjectId, $listProfileId, $deployId, $delivers, $opens);
-            $this->fromRepo->saveStats($fromId, $listProfileId, $deployId, $delivers, $opens);
-
+                // Currently list profile id is 0 because MT1 does not save that information
+                $listProfileId = 0;
+                $this->creativeRepo->saveStats($creativeId, $listProfileId, $deployId, $opens, $clicks);
+                $this->subjRepo->saveStats($subjectId, $listProfileId, $deployId, $delivers, $opens);
+                $this->fromRepo->saveStats($fromId, $listProfileId, $deployId, $delivers, $opens);
+            }
+            
         }
 
     }
