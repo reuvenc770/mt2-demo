d64bee793b28391f9a23bdeee8cdf6710fdfc47b
Patrick Cunningham <pcunningham@Patricks-MacBook-Pro.local>
2016-07-27 11:44:42 -0400

    added types

--- app/Models/MailingTemplate.php
+++ app/Models/MailingTemplate.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use App\Models\ModelTraits\ModelCacheControl;
 use Illuminate\Database\Eloquent\Model;
 
 class MailingTemplate extends Model
 {
+    use ModelCacheControl;
+    CONST NORMAL_HTML = ["id" => 0, "name" => "Normal Html"];
+    CONST HTML_LITE =  ["id" => 1, "name" => "HTML Lite (no images)"];
+    CONST IMAGE_ONLY =  ["id" => 2, "name" => "Image Only"];
+    CONST IMAGE_MAP =  ["id" => 3, "name" => "Image Map"];
+    CONST NEWSLETTER =  ["id" => 4, "name" => "Newsletter"];
+    CONST CLICK_BUTTON =  ["id" => 5, "name" => "Clickable Button"];
+
     protected $guarded = ['id'];
     protected $timestamps = false;
 
