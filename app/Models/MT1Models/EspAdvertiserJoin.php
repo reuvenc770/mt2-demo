be9172d2cb76452dc012df18b733538a8446284f
rbertorelli <rbertorelli@zetainteractive.com>
2016-07-27 19:12:26 +0000

    Repo and model for MT1's EspAdvertiserJoin

---
+++ app/Models/MT1Models/EspAdvertiserJoin.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\MT1Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EspAdvertiserJoin extends Model {
+    protected $connection = 'mt1mail';
+    protected $table = 'EspAdvertiserJoin';
+}
