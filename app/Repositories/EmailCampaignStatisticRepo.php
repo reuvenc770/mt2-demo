5cf932f49be5d8023e59441309a7ad33ccc277af
rbertorelli <rbertorelli@zetainteractive.com>
2016-07-29 14:06:27 +0000

    EmailCampaignStatisticRepo.php edited online with Bitbucket - prepending config name to config

--- app/Repositories/EmailCampaignStatisticRepo.php
+++ app/Repositories/EmailCampaignStatisticRepo.php
@@ -186,7 +186,7 @@ class EmailCampaignStatisticRepo {
     }
 
     protected function updateSuppressionStatus($email, $espInternalId, $field) {
-        $dataSchema = config('connections.mysql.database');
+        $dataSchema = config('database.connections.mysql.database');
         $this->model
              ->join("$dataSchema.emails as e", 'email_campaign_statistics.email_id', '=', 'e.id')
              ->join('standard_reports as sr', 'email_campaign_statistics.deploy_id', '=', 'sr.external_deploy_id')
