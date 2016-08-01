166918b7ddeb71c0b93384fd3fc4e708fba19843
Patrick Cunningham <pcunningham@Patricks-MacBook-Pro.local>
2016-08-01 12:36:49 -0400

    pr cleanup

--- app/Models/Deploy.php
+++ app/Models/Deploy.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Deploy extends Model
 {
     protected $guarded = ['id'];
+
+    public function ListProfiles(){
+        return $this->belongsToMany('App\Models\MT1Models\ListProfile',"list_profile","profile_id");
+    }
 }
--- database/migrations/2016_07_29_142011_create_deploys_table.php
+++ database/migrations/2016_07_29_142011_create_deploys_table.php
@@ -14,21 +14,32 @@ class CreateDeploysTable extends Migration
     {
         Schema::create('deploys', function (Blueprint $table) {
             $table->increments('id');
+            $table->string("deploy_name", 100);
             $table->date("send_date");
             $table->integer("esp_account_id");
-            $table->integer("list_profile_id");
+            $table->varchar("external_deploy_id", 100);
             $table->integer("offer_id");
             $table->integer("creative_id");
-            $table->integer("creative_from_id");
-            $table->integer("creative_subject_id");
-            $table->integer("creative_template_id");
+            $table->integer("from_id");
+            $table->integer("subject_id");
+            $table->integer("template_id");
             $table->integer("mailing_domain_id");
             $table->integer("content_domain_id");
             $table->integer("cake_instance_id");
+            $table->integer("cake_affiliate_id");
             $table->text("notes");
             $table->boolean("deployed");
         });
+
+        Schema::create('deploy_list_profile', function (Blueprint $table) {
+            $table->increments('deploy_id');
+            $table->string("list_profile_id");
+
+        });
     }
+
+
+
 
     /**
      * Reverse the migrations.
@@ -38,5 +49,6 @@ class CreateDeploysTable extends Migration
     public function down()
     {
         Schema::drop('deploys');
+        Schema::drop('deploy_list_profile');
     }
 }
