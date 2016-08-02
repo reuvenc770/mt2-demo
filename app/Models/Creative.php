82e4b6ef0a1461cf1a8be2762d852357cab4c9bc
rbertorelli <rbertorelli@zetainteractive.com>
2016-08-02 15:55:03 +0000

    Added fillable fields and fixed namespace for models

--- app/Models/Creative.php
+++ app/Models/Creative.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Creative extends Model
 {
-    //
+    protected $fillable = ['id', 'name', 'file_name', 'creative_html', 'approved', 'status'];
 }
--- app/Models/From.php
+++ app/Models/From.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class From extends Model
 {
-    //
+    protected $fillable = ['id', 'from_line', 'approved', 'status'];
 }
