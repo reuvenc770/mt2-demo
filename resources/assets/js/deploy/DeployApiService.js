75cfb3c928f0fa0eef6b5225d32790f40214b4f3
Patrick Cunningham <pcunningham@Patricks-MacBook-Pro.local>
2016-08-02 17:48:43 -0400

    offer type ahead

--- resources/assets/js/deploy/DeployApiService.js
+++ resources/assets/js/deploy/DeployApiService.js
@@ -3,7 +3,7 @@ mt2App.service( 'DeployApiService' , function ( $http , $log ) {
 
     self.baseApiUrl = '/api/deploy';
     self.baseEspApiUrl = '/api/espapi/all';
-
+    self.offerSearchUrl = '/api/offer/search/';
 
     self.getDeploys = function (successCallback, failureCallback) {
         $http({"method": "GET", "url": this.baseApiUrl})
@@ -13,6 +13,14 @@ mt2App.service( 'DeployApiService' , function ( $http , $log ) {
     self.getEspAccounts = function (successCallback, failCallBack){
         $http( { "method" : "GET" , "url" : this.baseEspApiUrl } )
             .then( successCallback , failCallBack );
+    };
+
+    self.getOffersSearch = function ( searchText , successCallback , failCallback ) {
+        $http( {
+            "method" : "GET" ,
+            "url" : this.offerSearchUrl ,
+            "params" : {searchTerm : searchText}
+        } ).then( successCallback , failCallback );
     };
 
 });
