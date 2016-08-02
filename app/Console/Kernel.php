6779a604f51cfe6efc55239cba5f3e3f444eb28f
rbertorelli <rbertorelli@zetainteractive.com>
2016-08-02 21:17:02 +0000

    Added jobs to kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -157,6 +157,9 @@ class Kernel extends ConsoleKernel
         $schedule->command('mt1Import offer')->dailyAt(self::MT1_SYNC_TIME);
         $schedule->command('mt1Import advertiser')->dailyAt(self::MT1_SYNC_TIME);
         $schedule->command('emails:download')->cron('*/2 * * * * *')->withoutOverlapping();
+        $schedule->command('mt1Import creative')->dailyAt(self::MT1_SYNC_TIME);
+        $schedule->command('mt1Import from')->dailyAt(self::MT1_SYNC_TIME);
+        $schedule->command('mt1Import subject')->dailyAt(self::MT1_SYNC_TIME);
 
         /**
          * Attribution Jobs
