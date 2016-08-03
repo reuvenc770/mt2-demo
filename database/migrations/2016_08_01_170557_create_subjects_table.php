edd9f99317924159bf40945a45aeceb306f08b77
rbertorelli <rbertorelli@zetainteractive.com>
2016-08-03 22:43:18 +0000

    Expanded migrations

--- database/migrations/2016_08_01_170557_create_subjects_table.php
+++ database/migrations/2016_08_01_170557_create_subjects_table.php
@@ -14,8 +14,23 @@ class CreateSubjectsTable extends Migration
         Schema::create('subjects', function (Blueprint $table) {
             $table->increments('id');
             $table->string('subject_line')->default('');
-            $table->boolean('approved')->default(0);
+            $table->boolean('is_approved')->default(0);
             $table->boolean('status')->default(0);
+
+            # Potential legacy fields
+            $table->integer('offer_id')->default(0);
+            $table->boolean('is_original')->default(0);
+            $table->date('date_approved')->nullable();
+            $table->string('approved_by', 20)->nullable();
+            $table->date('inactive_date')->nullable();
+            $table->boolean('internal_approved_flag')->default(0);
+            $table->dateTime('internal_date_approved')->nullable();
+            $table->string('internal_approved_by', 20)->nullable();
+            $table->boolean('copywriter')->default(0);
+            $table->string('copywriter_name', 5)->nullable();
+            $table->index('offer_id', 'offer_id');
+            $table->index('inactive_date', 'inactive_date');
+
             $table->timestamps();
         });
     }
