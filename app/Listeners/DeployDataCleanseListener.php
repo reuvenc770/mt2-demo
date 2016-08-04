d62e7b06715ea9f3fcff0ce76c1c2613f808fa9d
rbertorelli <rbertorelli@zetainteractive.com>
2016-08-04 16:14:36 +0000

    Removed unnecessary code in listener

--- app/Listeners/DeployDataCleanseListener.php
+++ app/Listeners/DeployDataCleanseListener.php
@@ -5,16 +5,11 @@ namespace App\Listeners;
 use App\Events\AttributionFileUploaded;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use App\Repositories\MT1Repositories\Mt1UniqueProfileRepo;
 use App\Events\NewDeployWasCreated;
 
 class DeployDataCleanseListener {
-  
-    private $uniqueProfileRepo;
-  
-    public function __construct(Mt1UniqueProfileRepo $uniqueProfileRepo) {
-        $this->uniqueProfileRepo = $uniqueProfileRepo;
-    }
+    
+    public function __construct() {}
   
     public function handle (NewDeployWasCreated $event) {
     
@@ -22,9 +17,7 @@ class DeployDataCleanseListener {
         $segments = $deploy->getSegments();
         
         foreach ($segments as $segment) {
-            $this->uniqueProfileRepo->setPull($segment->getId());
-
-            Artisan::call('the ftp dispatch job', ['segmentId' => $segment->getId()]);
+            // we do something here
         }    
     
     }
