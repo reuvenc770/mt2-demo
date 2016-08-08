09babbcb8a8a8ef66160a0a1ee13852b898c3bca
rbertorelli <rbertorelli@zetainteractive.com>
2016-08-08 19:52:49 +0000

    fixes for migration

--- database/migrations/2016_08_08_153106_create_offer_cfs_mapping_tables.php
+++ database/migrations/2016_08_08_153106_create_offer_cfs_mapping_tables.php
@@ -11,7 +11,7 @@ class CreateOfferCfsMappingTables extends Migration
      * @return void
      */
     public function up() {
-        Schema::connection('reporting')->create('offer_creative_maps', function (Blueprint $table) {
+        Schema::connection('reporting_data')->create('offer_creative_maps', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('offer_id')->default(0);
             $table->integer('creative_id')->default(0);
@@ -20,7 +20,7 @@ class CreateOfferCfsMappingTables extends Migration
             $table->index('creative_id', 'creative_id');
         });
 
-        Schema::connection('reporting')->create('offer_from_maps', function (Blueprint $table) {
+        Schema::connection('reporting_data')->create('offer_from_maps', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('offer_id')->default(0);
             $table->integer('from_id')->default(0);
@@ -29,7 +29,7 @@ class CreateOfferCfsMappingTables extends Migration
             $table->index('from_id', 'from_id');
         });
 
-        Schema::connection('reporting')->create('offer_subject_maps', function (Blueprint $table) {
+        Schema::connection('reporting_data')->create('offer_subject_maps', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('offer_id')->default(0);
             $table->integer('subject_id')->default(0);
@@ -40,17 +40,17 @@ class CreateOfferCfsMappingTables extends Migration
 
         // With the mapping tables, we no longer need these fields on the entity tables
 
-        Schema::connection('reporting')->table('creatives', function($table) {
+        Schema::table('creatives', function($table) {
             $table->dropIndex('offer_id');
             $table->dropColumn('offer_id');
         });
 
-        Schema::connection('reporting')->table('froms', function($table) {
+        Schema::table('froms', function($table) {
             $table->dropIndex('offer_id');
             $table->dropColumn('offer_id');
         });
 
-        Schema::connection('reporting')->table('subjects', function($table) {
+        Schema::table('subjects', function($table) {
             $table->dropIndex('offer_id');
             $table->dropColumn('offer_id');
         });
@@ -62,21 +62,21 @@ class CreateOfferCfsMappingTables extends Migration
      * @return void
      */
     public function down() {
-        Schema::connection('reporting')->drop('offer_creative_maps');
-        Schema::connection('reporting')->drop('offer_from_maps');
-        Schema::connection('reporting')->drop('offer_subject_maps');
+        Schema::connection('reporting_data')->drop('offer_creative_maps');
+        Schema::connection('reporting_data')->drop('offer_from_maps');
+        Schema::connection('reporting_data')->drop('offer_subject_maps');
 
-        Schema::connection('reporting')->table('creatives', function($table) {
+        Schema::table('creatives', function($table) {
             $table->integer('offer_id')->default(0)->after('id');
             $table->index('offer_id', 'offer_id');
         });
 
-        Schema::connection('reporting')->table('froms', function($table) {
+        Schema::table('froms', function($table) {
             $table->integer('offer_id')->default(0)->after('status');
             $table->index('offer_id', 'offer_id');
         });
 
-        Schema::connection('reporting')->table('subjects', function($table) {
+        Schema::table('subjects', function($table) {
             $table->integer('offer_id')->default(0)->after('status');
             $table->index('offer_id', 'offer_id');
         });
