62aa0881d2ab3df752bb2c6def502051972f6b17
Patrick Cunningham <pat@codedestroyer.com>
2016-08-22 15:14:19 -0400

    domain fixes

--- config/database.php
+++ config/database.php
@@ -116,7 +116,7 @@ return [
         'attribution' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('ATTR_DB_DATABASE', '' ), #Not using a default since we have multiple envs
+            'database'  => env('ATTR_DB_DATABASE', 'attribution' ), #Not using a default since we have multiple envs
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8mb4',
@@ -142,7 +142,7 @@ return [
         'slave_reports' => [
             'driver'    => 'mysql',
             'host'      => env('DB_SLAVE_HOST', 'localhost'),
-            'database'  => env('REPORTS_DB_DATABASE', '' ), #Not using a default since we have multiple envs
+            'database'  => env('REPORTS_DB_DATABASE', 'attribution' ), #Not using a default since we have multiple envs
             'username'  => env('DB_SLAVE_USERNAME', 'forge'),
             'password'  => env('DB_SLAVE_PASSWORD', ''),
             'charset'   => 'utf8mb4',
@@ -154,7 +154,7 @@ return [
         'slave_attribution' => [
             'driver'    => 'mysql',
             'host'      => env('DB_SLAVE_HOST', 'localhost'),
-            'database'  => env('ATTR_DB_DATABASE', '' ), #Not using a default since we have multiple envs
+            'database'  => env('ATTR_DB_DATABASE', 'attribution' ), #Not using a default since we have multiple envs
             'username'  => env('DB_SLAVE_USERNAME', 'forge'),
             'password'  => env('DB_SLAVE_PASSWORD', ''),
             'charset'   => 'utf8mb4',
---
+++ database/migrations/2016_08_22_135505_AddStatusToDomains.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStatusToDomains extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('domains', function (Blueprint $table) {
+            $table->boolean("status");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('domains', function (Blueprint $table) {
+            $table->dropColumn("status");
+        });
+    }
+}
