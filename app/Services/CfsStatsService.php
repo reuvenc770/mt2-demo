dfad702edcd6ce1a70e8eb9e198c8da49ae7451c
Patrick Cunningham <pat@codedestroyer.com>
2016-08-29 16:27:10 -0400

    finished previews

--- app/Services/CfsStatsService.php
+++ app/Services/CfsStatsService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Creative;
 use App\Repositories\CreativeClickthroughRateRepo;
 use App\Repositories\CreativeRepo;
 use App\Repositories\FromOpenRateRepo;
@@ -10,7 +11,7 @@ use App\Repositories\SubjectOpenRateRepo;
 use App\Repositories\SubjectRepo;
 //TODO: more Generic name, more specific repository?  or more another service same repo for crud actions on CFS
 class CfsStatsService {
-    
+
     private $creativeRepo;
     private $subjectRepo;
     private $fromLine;
@@ -44,7 +45,18 @@ class CfsStatsService {
 
 
     public function getCreativeByOfferId($offerId){
-       return $this->creativeRepo->getCreativesByOffer($offerId);
+       $creatives = $this->creativeRepo->getCreativesByOffer($offerId);
+
+        foreach($creatives as $creative){
+            $creative->creative_html = $this->parseReplaceHtml($creative->creative_html);
+        }
+        return $creatives;
+    }
+
+
+    private function parseReplaceHtml($html){
+       $html = preg_replace("/{{IMG_DOMAIN}}/", Creative::DEFAULT_DOMAIN, $html);
+        return $html;
     }
 
 
