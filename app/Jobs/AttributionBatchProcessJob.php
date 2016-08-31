2c4edc200d85600ee1f69532a812bd0737a9addb
rbertorelli <rbertorelli@zetainteractive.com>
2016-08-31 20:11:03 +0000

    New job to handle chunks of attribution records

---
+++ app/Jobs/AttributionBatchProcessJob.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Jobs\Traits\PreventJobOverlapping;
+use App\Factories\ServiceFactory;
+use App\Facades\JobTracking;
+use App\Models\JobEntry;
+use App\Events\AttributionCompleted;
+
+class AttributionBatchProcessJob extends Job implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels, PreventJobOverlapping;
+
+    private $jobName;
+    private $data;
+    private $modelId;
+    private $timestamp;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($data, $modelId, $timestamp, $tracking) {
+        $this->data = $data;
+        $this->modelId = $modelId;
+        $this->timestamp = $timestamp;
+        $this->jobName = 'AttributionBatchJob' . $modelId . $timestamp . $tracking;
+        JobTracking::startAggregationJob($this->jobName, $this->tracking);
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle() {
+
+        if ($this->jobCanRun($this->jobName)) {
+            try {
+                $this->createLock($this->jobName);
+                JobTracking::changeJobState(JobEntry::RUNNING, $this->tracking);
+                echo "{$this->jobName} running" . PHP_EOL;
+
+                $service = ServiceFactory::createAttributionBatchService($this->modelId);       
+                $service->process($this->data, $this->modelId, $this->timestamp);
+
+                JobTracking::changeJobState(JobEntry::SUCCESS, $this->tracking);
+            }
+            catch (\Exception $e) {
+                echo "{$this->jobName} failed with {$e->getMessage()}" . PHP_EOL;
+                $this->failed();
+            }
+            finally {
+                $this->unlock($this->jobName);
+            }
+        }
+
+        else {
+            echo "Still running {$this->jobName} - job level" . PHP_EOL;
+            JobTracking::changeJobState(JobEntry::SKIPPED, $this->tracking);
+        }
+
+    }
+
+    public function failed() {
+        JobTracking::changeJobState(JobEntry::FAILED, $this->tracking);
+        $this->unlock($this->jobName);
+    }
+}
---
+++ app/Services/AttributionBatchService.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Services;
+
+use Carbon\Carbon;
+use App\Repositories\EmailFeedAssignmentRepo;
+use App\Repositories\AttributionRecordTruthRepo;
+use App\Repositories\AttributionScheduleRepo;
+use App\Repositories\EmailFeedInstanceRepo;
+use Cache;
+
+class AttributionBatchService {
+    
+    private $expiringDay;
+    private $truthRepo;
+    private $scheduleRepo;
+    private $assignmentRepo;
+    private $feedInstanceRepo;
+    private $keyName = 'AttributionJob';
+
+    public function __construct(AttributionRecordTruthRepo $truthRepo, 
+                                AttributionScheduleRepo $scheduleRepo, 
+                                EmailFeedAssignmentRepo $assignmentRepo,
+                                EmailFeedInstanceRepo $feedInstanceRepo) {
+
+        $this->truthRepo = $truthRepo;
+        $this->scheduleRepo = $scheduleRepo;
+        $this->assignmentRepo = $assignmentRepo;
+        $this->feedInstanceRepo = $feedInstanceRepo;
+
+        $this->expiringDay = Carbon::today()->subDays(self::EXPIRATION_DAY_RANGE);
+    }
+
+
+    public function process($records, $modelId, $timestamp) {
+
+        $isModelRun = 'none' !== $modelId;
+
+        foreach ($records as $key => $record) {
+
+            $beginDate = $record->capture_date;
+            $feedId = (int)$record->feed_id;
+            $oldFeedId = (int)$record->feed_id;
+            $currentAttrLevel = (int)$record->level;
+
+            $hasAction = (bool)$record->has_action;
+            $actionExpired = $record->action_expired;
+            $subsequentImports = 0;
+
+            $potentialReplacements = $this->getPotentialReplacements($record->email_id, $beginDate, $feedId);
+
+            foreach ($potentialReplacements as $repl) {
+
+                if ($this->shouldChangeAttribution($beginDate, $hasAction, $actionExpired, $currentAttrLevel, $repl->level)) {
+                    $beginDate = $repl->capture_date;
+                    $currentAttrLevel = (int)$repl->level;
+                    $hasAction = 0; // by default must be false - can't switch if an action existed
+                    $feedId = (int)$repl->feed_id;
+                    $subsequentImports = 0;
+                    $actionExpired = 0; // again, can't have an action, so it can't be expired
+                }
+                else {
+                    $subsequentImports++;
+                }
+            }
+
+            // Only run this once we've found the winner
+            if ($oldFeedId !== $feedId) {
+                $this->changeAttribution($record->email_id, $feedId, $beginDate);
+        
+                if (!$isModelRun) {
+                    $this->recordHistory($record->email_id, $oldFeedId, $feedId);
+                    $this->updateScheduleTable($record->email_id, $beginDate);
+                    $this->updateTruthTable($record->email_id, $beginDate, $hasAction, $actionExpired, $subsequentImports);
+                }
+            }
+
+            Cache::decrement($this->keyName);
+
+            if (0 === (int)Cache::get($this->keyName)) {
+                Cache::forget($this->keyName);  // remove from redis
+
+                if ($thisIsModelRun) {
+                    // Attribution finished. Return model id and remove from storage
+                    \Event::fire(new AttributionCompleted($modelId)); // need model id
+                }
+            }
+
+        }, 50000);
+    }
+
+
+    protected function getPotentialReplacements($emailId, $beginDate, $feedId) {
+        if (0 !== $feedId) {
+            // assignment exists
+            return $this->feedInstanceRepo->getEmailInstancesAfterDate($emailId, $beginDate, $feedId);
+        }
+        else {
+            // going to create an assignment, so we need to get all instances
+            return $this->feedInstanceRepo->getInstances($emailId);
+        }
+        
+    }
+
+
+    protected function shouldChangeAttribution($captureDate, $hasAction, $actionExpired, $currentAttrLevel, $testAttrLevel) {
+
+        // needs to be explicitly checked - we don't just have the query to watch this
+        if ($this->expiringDay->gte(Carbon::parse($captureDate))) {
+            // Older than pre-defined X days ago
+            
+            if ($hasAction && $actionExpired) {
+                return true;
+            }
+            elseif (0 === $currentAttrLevel) {
+                // no attribution at all, so we need to set it
+                return true;
+            }
+            elseif (!$hasAction && $testAttrLevel < $currentAttrLevel) {
+                // "less than" here means "has a higher attribution level"
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function changeAttribution($emailId, $feedId, $captureDate) {
+        $this->assignmentRepo->assignFeed($emailId, $feedId, $captureDate);
+    }
+
+    protected function recordHistory($emailId, $oldFeedId, $newFeedId) {
+        $this->assignmentRepo->recordSwap($emailId, $oldFeedId, $newFeedId);
+    }
+
+    protected function updateTruthTable($emailId, $captureDate, $hasAction, $actionExpired, $subseqs) {
+        $addlImports = $subseqs >= 1;
+        $recentImport = Carbon::parse($captureDate)->gte($this->expiringDay);
+
+        $this->truthRepo->setRecord($emailId, $recentImport, $hasAction, $actionExpired, $addlImports);
+    }
+
+    protected function updateScheduleTable($emailId, $captureDate) {
+        // update schedule tables
+        $nextDate = Carbon::parse($captureDate)
+                          ->addDays(self::EXPIRATION_DAY_RANGE)
+                          ->format('Y-m-d');
+
+        $this->scheduleRepo->insertSchedule($emailId, $nextDate);
+    }
+}
