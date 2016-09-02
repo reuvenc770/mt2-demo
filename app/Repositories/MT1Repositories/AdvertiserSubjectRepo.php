e8cd7f3498c4c8027a0319c72e96a93ca2f56650
Patrick Cunningham <pat@codedestroyer.com>
2016-09-02 11:52:43 -0400

    full sync

--- app/Repositories/MT1Repositories/AdvertiserSubjectRepo.php
+++ app/Repositories/MT1Repositories/AdvertiserSubjectRepo.php
@@ -13,9 +13,6 @@ class AdvertiserSubjectRepo {
     }
 
     public function pullForSync($lookback) {
-        return $this->model
-                    ->whereNull('date_approved')
-                    ->orWhere('date_approved', '>=', DB::raw("CURDATE() - INTERVAL $lookback DAY"))
-                    ->get();
+        return $this->model->all();
     }
 }
