fa49de8b84c802b74c124857c2ba96c066e4be2c
rbertorelli <rbertorelli@zetainteractive.com>
2016-09-08 23:23:03 +0000

    Esp deploy field options model, migration, and seeder

---
+++ app/Models/EspFieldOption.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EspFieldOption extends Model{
+    
+    protected $primaryKey = 'esp_id';
+    protected $guarded = [];
+
+    public function esp() {
+        return $this->belongsTo('App\Models\Esp');
+    }
+}
