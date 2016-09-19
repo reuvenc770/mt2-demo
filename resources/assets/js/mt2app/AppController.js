f6cf360b6e98b43346c160a721f3c3c8ef830c3e
Melissa Lin <mlin@zetainteractive.com>
2016-09-19 21:54:09 +0000

    update sidenav status to be persistent across all pages

--- resources/assets/js/mt2app/AppController.js
+++ resources/assets/js/mt2app/AppController.js
@@ -1,6 +1,6 @@
-mt2App.controller( 'AppController' , [ '$rootScope' , '$location' , '$window' , '$mdSidenav' , '$mdToast' , '$mdMedia' , '$log' , function ( $rootScope , $location , $window , $mdSidenav , $mdToast , $mdMedia , $log ) {
+mt2App.controller( 'AppController' , [ '$rootScope' , '$location' , '$window' , '$mdSidenav' , '$mdToast' , '$mdMedia' , '$cookies' , '$log' , function ( $rootScope , $location , $window , $mdSidenav , $mdToast , $mdMedia , $cookies , $log ) {
     var self = this;
-    self.lockSideNav = true;
+    self.lockSidenav = true;
 
     self.getBaseUrl = function () {
         return $location.protocol() + '://' + $location.host() + '/';
@@ -16,7 +16,15 @@ mt2App.controller( 'AppController' , [ '$rootScope' , '$location' , '$window' , 
         $window.location.href = redirectURL;
     };
 
-    self.toggleMenu = function ( navId ) { $mdSidenav( navId ).toggle(); }
+    self.toggleMenu = function ( navId , event ) {
+        if ( self.largePageWidth() ){
+            self.lockSidenav = !self.lockSidenav;
+
+            self.setSidenavCookie( event );
+        } else {
+            $mdSidenav( navId ).toggle();
+        }
+    };
 
     self.openDropdownMenu = function( $mdOpenMenu , ev ) {
         originatorEv = ev;
@@ -36,4 +44,15 @@ mt2App.controller( 'AppController' , [ '$rootScope' , '$location' , '$window' , 
 
         $mdToast.show( toast );
     };
+
+    self.setSidenavCookie = function( event ) {
+        var currentSidenavCookie = $cookies.get('sidenavCookie');
+
+        if ( typeof(currentSidenavCookie) != 'undefined' && typeof(event) == 'undefined' ) {
+            self.lockSidenav = currentSidenavCookie === 'true' ? true : false;
+        } else {
+            $cookies.put( 'sidenavCookie' , self.lockSidenav );
+        }
+    };
+
 } ] );
