eb315637c3fb0582e31d9ce33957008c89194bf0
rbertorelli <rbertorelli@zetainteractive.com>
2016-09-30 18:03:47 +0000

    Migrations for new table and modification to cake_aggregated_data table to add key useful for query

---
+++ app/Models/EmailActionAggregation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmailActionAggregation extends Model
+{
+    protected $connection = 'list_profile';
+    protected $guarded = [];
+}
---
+++ app/Repositories/EmailActionAggregationRepo.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\EmailActionAggregation;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Query\Builder;
+
+/**
+ *
+ */
+class EmailActionAggregationRepo {
+  
+    private $aggregation;
+    private $batchData = [];
+    private $batchDataSize;
+    const MAX_INSERT_SIZE = 10000;
+
+    public function __construct(EmailActionAggregation $aggregation) {
+        $this->aggregation = $aggregation;
+        $this->batchDataSize = 0;
+    } 
+
+    public function insertBatch($data) {
+        if (self::MAX_INSERT_SIZE === $this->batchDataSize) {
+            // insert
+            $this->insertBatchData();
+
+            // set batchData to data
+            $this->batchData = [$this->prepareData($data)];
+            $this->batchDataSize = 1;
+        }
+        else {
+            // Merely insert into holding array
+            $this->batchData[] = $this->prepareData($data);
+            $this->batchDataSize++;
+        }
+    }
+
+
+    public function cleanUpBatch() {
+        // insert
+        $this->insertBatchData();
+    }
+
+    private function prepareData($row) {
+        return "('{$this->email_id}', '{$this->deploy_id}', 
+            '{$this->date}', '{$this->deliveries}', '{$this->opens}', 
+            '{$this->clicks}', '{$this->conversions}', NOW(), NOW())";
+    }
+
+    private function insertBatchData() {
+        if ($this->batchDataSize > 0) {
+            $schema = config('database.connections.list_profile.database');
+
+            $inserts = implode(',', $this->batchData);
+
+            DB::statement("INSERT INTO $schema.email_action_aggregations 
+                (email_id, deploy_id, date, deliveries, opens, clicks, conversions, created_at, updated_at)
+
+                VALUES $inserts
+
+                ON DUPLICATE KEY UPDATE
+                email_id = email_id,
+                deploy_id = deploy_id,
+                date = date,
+                deliveries = VALUES(deliveries),
+                opens = VALUES(opens),
+                clicks = VALUES(clicks),
+                conversions = VALUES(conversions),
+                created_at = created_at,
+                updated_at = NOW()");
+        }    
+    }
+
+}
