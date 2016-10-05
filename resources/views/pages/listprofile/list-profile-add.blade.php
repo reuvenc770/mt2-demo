f12a9b23bad36656166dfd50a2eb6cf004eb5ba0
Melissa Lin <mlin@zetainteractive.com>
2016-10-05 16:52:43 +0000

    create listprofile form skeleton

--- resources/views/pages/listprofile/list-profile-add.blade.php
+++ resources/views/pages/listprofile/list-profile-add.blade.php
@@ -8,6 +8,24 @@
 @stop
 
 @section( 'content' )
+<md-content layout="row" layout-align="center center" class="md-mt2-zeta-theme md-hue-1">
+   <div flex-gt-sm="60" flex="100">
+
+         @include( 'pages.listprofile.list-profile-form' )
+
+         <div layout="row" layout-align="end end">
+             <md-button layout="row" class="md-raised md-accent">
+                 <md-icon class="material-icons" md-font-set="material-icons">archive</md-icon>
+                 <span flex> Export to FTP</span>
+             </md-button>
+             <md-button layout="row" class="md-raised md-accent">
+                 <md-icon class="material-icons" md-font-set="material-icons">save</md-icon>
+                 <span flex> Save</span>
+             </md-button>
+         </div>
+
+   </div>
+</md-content>
 @stop
 
 @section( 'pageIncludes' )
---
+++ resources/views/pages/listprofile/list-profile-form.blade.php
@@ -0,0 +1,47 @@
+<form name="listProfileForm" layout="column" novalidate>
+
+   <md-card>
+      <md-toolbar>
+         <div class="md-toolbar-tools"><span>List Profile</span></div>
+      </md-toolbar>
+      <md-card-content>
+
+      </md-card-content>
+
+      <md-divider></md-divider>
+
+      <md-card-content>
+         <!-- range/action fields -->
+      </md-card-content>
+
+      <md-divider></md-divider>
+
+      <md-card-content>
+         <!-- ISP/category/offer fields -->
+      </md-card-content>
+
+      <md-toolbar>
+         <div class="md-toolbar-tools"><span>Attribute Filtering</span></div>
+      </md-toolbar>
+
+      <md-card-content>
+
+      </md-card-content>
+
+      <md-toolbar>
+         <div class="md-toolbar-tools"><span>Suppression</span></div>
+      </md-toolbar>
+      <md-card-content>
+
+      </md-card-content>
+
+      <md-toolbar>
+         <div class="md-toolbar-tools"><span>Hygiene</span></div>
+      </md-toolbar>
+      <md-card-content>
+
+      </md-card-content>
+
+   </md-card>
+
+</form>
