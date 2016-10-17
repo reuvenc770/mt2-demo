e2c3944e8d4f16c74bb6c047cf84a59612d60bd5
rbertorelli <rbertorelli@zetainteractive.com>
2016-10-17 19:50:30 +0000

    Expanded list profile table, new getter in repo

--- app/Models/ListProfile.php
+++ app/Models/ListProfile.php
@@ -8,4 +8,25 @@ class ListProfile extends Model
 {
     protected $guarded = [''];
     public $timestamps = false;
+    protected $connection = 'list_profile';
+
+    public function clients() {
+        return $this->belongsToMany('App\Models\Client', 'list_profile.list_profile_clients');
+    }
+
+    public function feeds() {
+        return $this->belongsToMany('App\Models\Feed', 'list_profile.list_profile_feeds');
+    }
+
+    public function domainGroups() {
+        return $this->belongsToMany('App\Models\DomainGroup', 'list_profile.list_profile_domain_groups');
+    }
+
+    public function offers() {
+        return $this->belongsToMany('App\Models\Offer', 'list_profile.list_profile_offers');
+    }
+
+    public function verticals() {
+        return $this->belongsToMany('App\Models\CakeVertical', 'list_profile.list_profile_verticals');
+    }
 }
---
+++ database/migrations/2016_10_12_095714_expand_list_profile_schema.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ExpandListProfileSchema extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::connection('list_profile')->table('list_profiles', function(Blueprint $table) {
+            $table->dropColumn('attributes');
+            $table->boolean('use_global_suppression')->default(1)->after('conversion_count');
+            $table->json('age_range')->after('use_global_suppression');
+            $table->json('gender')->after('age_range');
+            $table->json('zip')->after('gender');
+            $table->json('city')->after('zip');
+            $table->json('state')->after('city');
+            $table->json('device_type')->after('state');
+            $table->json('device_os')->after('device_type');
+            $table->json('mobile_carrier')->after('device_type');
+            $table->boolean('insert_header')->default(0)->after('mobile_carrier');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::connection('list_profile')->table('list_profiles', function(Blueprint $table) {
+            $table->dropColumn('age_range');
+            $table->dropColumn('gender');
+            $table->dropColumn('zip');
+            $table->dropColumn('city');
+            $table->dropColumn('state');
+            $table->dropColumn('device_type');
+            $table->dropColumn('device_os');
+            $table->dropColumn('mobile_carrier');
+            $table->dropColumn('insert_header');
+
+            $table->json('attributes')->after('device_type');
+        });
+    }
+}
