7adb517a98bd5068ad2c62df4b009fd8e53c4996
Adam Chin <achin@zetainteractive.com>
2016-10-26 20:58:49 +0000

    Committing fix for an error Rob saw on production.

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -3,8 +3,11 @@
 namespace App\Services;
 
 use App\Repositories\ClientRepo;
+use App\Services\Interfaces\IFtpAdmin;
+use Artisan;
+use DB;
 
-class ClientService
+class ClientService implements IFtpAdmin
 {
     private $repo;
 
@@ -16,4 +19,34 @@ class ClientService
         return $this->repo->get();
     }
 
+    public function saveFtpUser ( $credentials ) {
+        Log::info( 'Saving user credentials to db. Creds: ' . json_encode( $credentials ) );
+
+        DB::connection( 'mt1_data' )->table( 'user' )
+            ->where( 'username' , $credentials[ 'username' ] )
+            ->update( [ 'ftp_pw' => $credentials[ 'password' ],
+                'ftp_user' => $credentials[ 'username' ],
+                'ftp_url' => $credentials['ftp_url'],
+                'newClient' => 0 ] );
+    }
+
+    public function findNewFtpUsers () {
+        return DB::connection( 'mt1_data' )->table( 'user' )
+            ->select( 'username' )
+            ->where( [ 'newClient' => 1 , 'ftp_user' => '' ] )
+            ->get();
+    }
+
+    public function resetPassword($username){
+        Artisan::queue('ftp:admin', [
+            '-H' => "52.205.67.250",
+            '-U' => 'root',
+            '-k' => '~/.ssh/mt2ftp.pub',
+            '-K' => '~/.ssh/mt2ftp',
+            '-u' => $username,
+            '-s' => "Client",
+            '-r' => true
+        ]);
+        return true;
+    }
 }
