12ed9e9660f8802d78ec144fc937f67eb6438f8f
Adam Chin <achin@zetainteractive.com>
2016-11-16 19:40:30 +0000

    Changed it to pull records for the past N days specified in command options.

--- app/Repositories/MT1Repositories/SuppressListOrangeRepo.php
+++ app/Repositories/MT1Repositories/SuppressListOrangeRepo.php
@@ -6,11 +6,8 @@
 namespace App\Repositories\MT1Repositories;
 
 use App\Models\MT1Models\SuppressListOrange;
-use App\Models\EtlPickup;
 
 class SuppressListOrangeRepo {
-    const LAST_ID_FIELD = 'SuppressListOrangeId';
-
     protected $model;
 
     public function __construct ( SuppressListOrange $model ) {
@@ -18,11 +15,8 @@ class SuppressListOrangeRepo {
     }
 
     public function pullForSync ( $lookback ) {
-        $etlPickup = new EtlPickup();
-        $id = $etlPickup->where('name', self::LAST_ID_FIELD)->first()->stop_point;
-
         return $this->model
                     ->join( 'SuppressionReason as sr' , 'suppress_list_orange.suppressionReasonID' , '=' , 'sr.suppressionReasonID' )
-                    ->where( 'sid' , '>=' , $id );
+                    ->where( 'dateTimeSuppressed' , '>=' , DB::raw("CURDATE() - INTERVAL $lookback DAY") );
     }
 }
