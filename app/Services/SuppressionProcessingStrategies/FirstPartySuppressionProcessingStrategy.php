991ce04671ce366258107e6e84c62fb9a6db7e58
rbertorelli <rbertorelli@zetainteractive.com>
2016-11-24 00:01:38 +0000

    Simplifying suppression processing - looks like we only need one (for now) per-party

--- app/Services/SuppressionProcessingStrategies/FirstPartySuppressionProcessingStrategy.php
+++ app/Services/SuppressionProcessingStrategies/FirstPartySuppressionProcessingStrategy.php
@@ -3,15 +3,15 @@
 namespace App\Services\SuppressionProcessingStrategies;
 
 use App\Repositories\FirstPartyOnlineSuppressionListRepo;
-use App\Services\BrontoReportService;
+use App\Services\AbstractReportService;
 
-class SimplyJobsSuppressionProcessingStrategy implements ISuppressionProcessingStrategy {
+class FirstPartySuppressionProcessingStrategy implements ISuppressionProcessingStrategy {
 
     private $onlineListRepo;
     private $onlineListMap;
     private $apiService;
 
-    public function __construct(FirstPartyOnlineSuppressionListRepo $onlineListRepo, BrontoReportService $apiService) {
+    public function __construct(FirstPartyOnlineSuppressionListRepo $onlineListRepo, AbstractReportService $apiService) {
         $this->onlineListRepo = $onlineListRepo;
         $this->apiService = $apiService;
     }
@@ -26,15 +26,10 @@ class SimplyJobsSuppressionProcessingStrategy implements ISuppressionProcessingS
 
     public function processSuppression($supp) {
         $lists = $this->formatLists($supp->suppression_lists);
-
-        // maybe this isn't enough?
-        $this->apiService->addContact([
-            'email' => $supp->email_address, 
-            'listIds' => $lists
-        ]);
+        $this->apiService->addContactToLists($supp->email_address, $lists);
 
         /**
-            Need to set email template stuff here
+            Need to set email template stuff here for SimplyJobs
         */
 
     }
