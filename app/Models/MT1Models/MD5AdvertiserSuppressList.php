480366e32f6b5c6e3ab81c9ba6131825703f195f
Adam Chin <achin@zetainteractive.com>
2016-12-02 18:40:46 +0000

    removed compound primary key from md5 supp model.

--- app/Models/MT1Models/MD5AdvertiserSuppressList.php
+++ app/Models/MT1Models/MD5AdvertiserSuppressList.php
@@ -10,5 +10,4 @@ use Illuminate\Database\Eloquent\Model;
 class MD5AdvertiserSuppressList extends Model {
     protected $connection = "mt1supp";
     protected $table = "MD5AdvertiserSuppressList";
-    protected $primaryKey = [ 'advertiser_id' , 'md5sum' ];
 }
