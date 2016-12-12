81812000a54ea4b66c1e546eee4d26c61d762f09
Melissa Lin <mlin@zetainteractive.com>
2016-12-12 20:00:03 +0000

    add tooltips to append eid

--- resources/views/bootstrap/pages/appendeid/append-eid-index.blade.php
+++ resources/views/bootstrap/pages/appendeid/append-eid-index.blade.php
@@ -9,17 +9,21 @@
             <div class="panel-title">Append EID</div>
         </div>
         <div class="panel-body">
+            <p>To get information associated to an EID, upload EID list file and select the information you would like returned. If nothing is selected, only email address will be returned. Output file will be saved to FTP.</p>
+            <br/>
             <fieldset>
-                <div class="form-group">
-                    <md-switch ng-model="append.feed" aria-label="Include Current Feed Name?">
-                        Include Current Feed Name?
-                    </md-switch>
-                    <md-switch ng-model="append.fields" aria-label=" Include Email Information">
-                        Append all fields
-                    </md-switch>
-                    <md-switch ng-model="append.suppress" aria-label=" Include Suppressed Records">
-                        Include inactive/suppressed records
-                    </md-switch>
+                <div layout="column">
+                    <md-checkbox ng-model="append.feed" aria-label="Include current feed name.">
+                        Include current feed name.
+                    </md-checkbox>
+                    <md-checkbox ng-model="append.fields" aria-label="Include email information">
+                        Include all data fields.
+                        <md-icon md-font-set="material-icons" class="mt2-icon-black material-icons icon-xs cmp-tooltip-marker" data-toggle="popover" data-placement="right" data-content="If box is checked, all email information for each EID record will be included (ie: name, address, gender, etc).">help</md-icon>
+                    </md-checkbox>
+                    <md-checkbox ng-model="append.suppress" aria-label=" Include suppressed records">
+                        Include inactive/suppressed records.
+                        <md-icon md-font-set="material-icons" class="mt2-icon-black material-icons icon-xs cmp-tooltip-marker" data-toggle="popover" data-placement="right" data-content="If the uploaded EID list has inactive/suppressed records and this is checked, the return file will include these records. If unchecked, inactive/suppressed records will be removed. ">help</md-icon>
+                    </md-checkbox>
                 </div>
                 <div class="btn-group" flow-init="{ target : 'api/attachment/upload' , query : { 'fromPage' : 'appendEID' , '_token' : '{{ csrf_token() }}' } }"
                     flow-files-submitted="$flow.upload()"
