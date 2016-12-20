f48fc8716429ce7f0a88805c6a2cd5ed8a10dd31
Adam Chin <achin@zetainteractive.com>
2016-12-20 20:49:18 +0000

    Committing migration

---
+++ database/migrations/2016_12_20_144537_create_content_server_stats_raws_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentServerStatsRawsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('content_server_stats_raws', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger( 'eid' )->unsigned();
+            $table->bigInteger( 'link_id' )->unsigned();
+            $table->integer( 'sub_aff_id' )->unsigned();
+            $table->tinyInteger( 'is_open' )->unsigned()->default( 0 );
+            $table->tinyInteger( 'is_click' )->unsigned()->default( 0 );
+            $table->text( 'user_agent' );
+            $table->text( 'referrer' );
+            $table->text( 'query_string' );
+            $table->dateTime( 'action_datetime' );
+
+            $table->index( 'eid' );
+            $table->index( 'link_id' );
+            $table->index( 'sub_aff_id' );
+            $table->index( 'is_open' );
+            $table->index( 'is_click' );
+            $table->index( 'action_datetime' );
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('content_server_stats_raws');
+    }
+}
