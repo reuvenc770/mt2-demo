519c7ab13992f776f1c4579a7d18dccedc319aff
Adam Chin <achin@zetainteractive.com>
2016-12-21 20:14:40 +0000

    forgot to include the model.

--- app/Repositories/AttributionExpirationScheduleRepo.php
+++ app/Repositories/AttributionExpirationScheduleRepo.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Repositories\AttributionScheduleRepo;
+use App\Models\AttributionExpirationSchedule;
 
 class AttributionExpirationScheduleRepo extends AttributionScheduleRepo {
     public function __construct ( AttributionExpirationSchedule $schedule ) {
