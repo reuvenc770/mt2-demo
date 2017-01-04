00b4c41d1886a696a3e6e576f74357aac2048f67
rbertorelli <rbertorelli@zetainteractive.com>
2017-01-04 15:48:49 +0000

    misc.php edited online with Bitbucket - hotfix missing semicolon

--- config/misc.php
+++ config/misc.php
@@ -4,4 +4,4 @@ return [
     'a_weber_debug' => env('AWEBER_DEBUG', false),
     'esp_cake_redir_domain' => env('ESP_CAKE_REDIR_DOMAIN', ''),
     'cake_old_redir_domain' => env('CAKE_OLD_REDIR_DOMAIN')
-]
+];
