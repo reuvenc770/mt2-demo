a48b1f579422db32a9eee89b69bc9641730e7184
Melissa Lin <mlin@zetainteractive.com>
2017-01-10 17:43:01 +0000

    merge domaingroup bootstrap assets to normal assets

--- resources/assets/js/domaingroup/DomainGroupApiService.js
+++ resources/assets/js/domaingroup/DomainGroupApiService.js
@@ -1,4 +1,4 @@
-mt2App.service( 'DomainGroupApiService' , function ( $http , $log ) {
+mt2App.service( 'DomainGroupApiService' , [ 'paginationService' , '$http' , '$log' , function ( paginationService , $http , $log ) {
     var self = this;
 
     self.baseApiUrl = '/api/ispgroup';
@@ -10,12 +10,7 @@ mt2App.service( 'DomainGroupApiService' , function ( $http , $log ) {
     };
 
     self.getAccounts = function ( page , count , sortField , successCallback , failureCallback ) {
-        var sort = { 'field' : sortField , 'desc' : false };
-
-        if (/^\-/.test( sortField ) ) {
-            sort.field = sort.field.substring( 1 );
-            sort.desc = true;
-        }
+        var sort = paginationService.sortPage( sortField );
 
         return $http( {
             "method" : "GET" ,
@@ -42,5 +37,12 @@ mt2App.service( 'DomainGroupApiService' , function ( $http , $log ) {
         } ).then( successCallback , failureCallback );
     };
 
+    self.toggleRow = function ( recordId , direction , successCallback , failureCallback ) {
+        $http( {
+            "method" : "DELETE" ,
+            "url" : this.baseApiUrl + '/' + recordId,
+            "params" : { "direction" : direction }
+        } ).then( successCallback , failureCallback );
+    };
 
-} );
+} ] );
