f06a9bf076273f8ee97cff7a595650ee18089919
rbertorelli <rbertorelli@zetainteractive.com>
2017-01-11 23:26:12 +0000

    Redshift repos and interface

---
+++ app/Repositories/RedshiftRepositories/FeedRepo.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repositories\RedshiftRepositories;
+
+use App\Models\RedshiftModels\Feed;
+use App\Repositories\RepoInterfaces\IRedshiftRepo;
+use DB;
+
+class FeedRepo implements IRedshiftRepo {
+    
+    private $model;
+
+    public function __construct(Feed $model) {
+        $this->model = $model;
+    }
+
+    public function loadEntity($fileName) {
+        $sql = <<<SQL
+copy feeds
+from 's3://mt2-listprofile-export/{$fileName}.csv'
+credentials 'aws_iam_role=arn:aws:iam::286457008090:role/redshift-s3-stg'
+format as csv quote as '"' delimiter as ',';
+SQL;
+        DB::connection('redshift')->statement($sql);
+    }
+}
