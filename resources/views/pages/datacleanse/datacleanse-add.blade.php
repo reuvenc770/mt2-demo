985511a5a5f950e23d1711764edf38f9a90c1824
Melissa Lin <mlin@zetainteractive.com>
2017-01-12 15:26:11 +0000

    merge datacleanse bootstrap assets to normal assets

--- resources/views/pages/datacleanse/datacleanse-add.blade.php
+++ resources/views/pages/datacleanse/datacleanse-add.blade.php
@@ -3,30 +3,27 @@
 @section( 'title' , 'Add Data Cleanse' )
 
 @section( 'content' )
-
 <div ng-controller="DataCleanseController as cleanse">
-    <md-content layout="row" layout-align="center center" class="md-mt2-zeta-theme md-hue-1">
-        <div flex-gt-sm="50" flex="100">
-            <div layout="column" layout-align="end end">
-               <md-button class="md-raised md-accent" ng-disabled="cleanse.creatingCleanse" ng-click="cleanse.saveCleanse( $event , cleanseForm )" layout="row">
-                  <md-icon md-font-set="material-icons" class="mt2-icon-black" ng-hide="cleanse.creatingCleanse">save</md-icon>
-                  <md-progress-circular ng-show="cleanse.creatingCleanse" md-mode="indeterminate" md-diameter="16"></md-progress-circular><span flex>Save</span>
-               </md-button>
-            </div>
+    <form name="cleanseForm" novalidate>
+        <button type="button" class="btn btn-primary btn-md pull-right padding" ng-class="{ 'disabled' : cleanse.creatingCleanse }" ng-click="cleanse.saveCleanse( $event , cleanseForm )" layout="row">
+            <md-icon md-font-set="material-icons" ng-hide="cleanse.creatingCleanse">save</md-icon>
+            <span class="glyphicon glyphicon-repeat" ng-show="cleanse.creatingCleanse" ng-class="{ 'rotateMe' : cleanse.creatingCleanse }"></span>
+            Save
+        </button>
 
-            @include( 'pages.datacleanse.datacleanse-form' )
+        <div class="clearfix"></div>
 
-            <div layout="column" layout-align="end end">
-               <md-button class="md-raised md-accent" ng-disabled="cleanse.creatingCleanse" ng-click="cleanse.saveCleanse( $event , cleanseForm )" layout="row">
-                  <md-icon md-font-set="material-icons" class="mt2-icon-black" ng-hide="cleanse.creatingCleanse">save</md-icon>
-                  <md-progress-circular ng-show="cleanse.creatingCleanse" md-mode="indeterminate" md-diameter="16"></md-progress-circular><span flex>Save</span>
-               </md-button>
-            </div>
-        </div>
-    </md-content>
+        @include( 'pages.datacleanse.datacleanse-form' )
+
+        <button type="button" class="btn btn-primary btn-md pull-right" ng-class="{ 'disabled' : cleanse.creatingCleanse }" ng-click="cleanse.saveCleanse( $event , cleanseForm )">
+            <md-icon md-font-set="material-icons" ng-hide="cleanse.creatingCleanse">save</md-icon>
+            <span class="glyphicon glyphicon-repeat" ng-show="cleanse.creatingCleanse" ng-class="{ 'rotateMe' : cleanse.creatingCleanse }"></span>
+            Save
+        </button>
+    </form>
 </div>
 @stop
 
-@section( 'pageIncludes' )
-<script src="js/datacleanse.js"></script>
-@stop
+<?php Assets::add(
+        ['resources/assets/js/datacleanse/DataCleanseController.js',
+                'resources/assets/js/datacleanse/DataCleanseApiService.js'],'js','pageLevel') ?>
--- resources/views/pages/datacleanse/datacleanse-index.blade.php
+++ resources/views/pages/datacleanse/datacleanse-index.blade.php
@@ -6,45 +6,40 @@
 
 @section( 'page-menu' )
     @if ( Sentinel::hasAccess( 'datacleanse.add' ) )
-        <md-button ng-click="cleanse.viewAdd()" aria-label="Add Data Cleanse">
-            <md-icon md-font-set="material-icons" class="mt2-icon-black" ng-show="app.isMobile()">add_circle_outline</md-icon>
-            <span ng-hide="app.isMobile()">Add Data Cleanse</span>
-        </md-button>
+        <li><a ng-click="cleanse.viewAdd()">Add Data Cleanse</a></li>
     @endif
 @stop
 
 @section( 'content' )
 <div ng-init="cleanse.load()">
-    <md-content layout="row" layout-align="center" class="md-mt2-zeta-theme md-hue-1">
-        <md-card flex-gt-md="70" flex="100">
-            <md-table-container>
-                <table md-table md-progress="cleanse.queryPromise">
-                    <thead md-head >
-                        <tr md-row>
-                            <th md-column class="md-table-header-override-whitetext">File Name</th>
-                            <th md-column class="md-table-header-override-whitetext">Updated</th>
-                            <th md-column class="md-table-header-override-whitetext">Records</th>
-                        </tr>
-                    </thead>
+    <md-card>
+        <md-table-container>
+            <table md-table md-progress="cleanse.queryPromise">
+                <thead md-head >
+                    <tr md-row>
+                        <th md-column class="md-table-header-override-whitetext">File Name</th>
+                        <th md-column class="md-table-header-override-whitetext">Updated</th>
+                        <th md-column class="md-table-header-override-whitetext">Records</th>
+                    </tr>
+                </thead>
 
-                    <tbody md-body>
-                        <tr md-row ng-repeat="record in cleanse.cleanses track by $index">
-                            <td md-cell>@{{ record.name }}</td>
-                            <td md-cell>@{{ record.lastUpdated }}</td>
-                            <td md-cell>@{{ record.count }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </md-table-container>
+                <tbody md-body>
+                    <tr md-row ng-repeat="record in cleanse.cleanses track by $index">
+                        <td md-cell>@{{ record.name }}</td>
+                        <td md-cell>@{{ record.lastUpdated }}</td>
+                        <td md-cell>@{{ record.count }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </md-table-container>
 
-            <md-content class="md-mt2-zeta-theme md-hue-2">
-                <md-table-pagination md-limit="cleanse.paginationCount" md-limit-options="[10, 25, 50, 100]" md-page="cleanse.currentPage" md-total="@{{cleanse.cleanseTotal}}" md-on-paginate="cleanse.load" md-page-select></md-table-pagination>
-            </md-content>
-        </md-card>
-    </md-content>
+        <md-content class="md-mt2-zeta-theme md-hue-2">
+            <md-table-pagination md-limit="cleanse.paginationCount" md-limit-options="[10, 25, 50, 100]" md-page="cleanse.currentPage" md-total="@{{cleanse.cleanseTotal}}" md-on-paginate="cleanse.load" md-page-select></md-table-pagination>
+        </md-content>
+    </md-card>
 </div>
 @stop
 
-@section( 'pageIncludes' )
-<script src="js/datacleanse.js"></script>
-@stop
+<?php Assets::add(
+        ['resources/assets/js/datacleanse/DataCleanseController.js',
+                'resources/assets/js/datacleanse/DataCleanseApiService.js'],'js','pageLevel') ?>
