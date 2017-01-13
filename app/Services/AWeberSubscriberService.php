e249e8d0fa6d3d3c3f290f32bccc983e7b5f4564
Patrick Cunningham <pat@codedestroyer.com>
2017-01-13 17:24:50 -0500

    fixing count check

--- app/Services/AWeberSubscriberService.php
+++ app/Services/AWeberSubscriberService.php
@@ -14,7 +14,7 @@ use App\Services\API\AWeberApi;
  */
 class AWeberSubscriberService
 {
-    CONST INSERT_COUNT = 20;
+    CONST INSERT_COUNT = 300;
     protected $api;
     protected $subscribers = [];
     protected $subscriberRepo;
@@ -65,16 +65,13 @@ class AWeberSubscriberService
     }
 
     public function queueSubscriber($subscriber){
-        print_r($subscriber);
         $this->subscribers[] =  "( "
             . join( " , " , [
                 '"'.$subscriber->email.'"' ,
                 $subscriber->id] )
             . " )";
-        $this->count++;
-        if($this->count >= self::INSERT_COUNT){
+        if (self::INSERT_COUNT <= sizeof($this->subscribers)) {
             $this->insertSubscribers();
-            $this->count = 0;
         }
     }
     
