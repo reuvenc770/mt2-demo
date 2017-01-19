c4cfa4e8f2d2c1552ba0d16bfa1298006dee67e6
rbertorelli <rbertorelli@zetainteractive.com>
2017-01-19 21:44:42 +0000

    Backdating some keys on large tables

--- database/migrations/2016_06_29_110749_create_email_client_assignments_table.php
+++ database/migrations/2016_06_29_110749_create_email_client_assignments_table.php
@@ -18,11 +18,13 @@ class CreateEmailClientAssignmentsTable extends Migration
         Schema::connection( 'attribution' )->create('email_client_assignments', function (Blueprint $table) {
             $table->bigInteger( 'email_id' )->unsigned();
             $table->integer( 'client_id' )->unsigned();
-            $table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
 
             $table->primary( 'email_id' );
             $table->index( 'client_id' );
             $table->index( [ 'email_id' , 'client_id' ] );
+            $table->index('updated_at', 'updated_at');
         });
     }
 
