d8e8d372b76b340c40cc08396a25040e694d90f4
Patrick Cunningham <pat@codedestroyer.com>
2017-01-26 16:16:09 -0500

    typo

--- database/migrations/2017_01_24_161905_AddDatabaseandDimaTables.php
+++ database/migrations/2017_01_24_161905_AddDatabaseandDimaTables.php
@@ -25,7 +25,7 @@ class AddDatabaseandDimaTables extends Migration
             $table->integer("esp_account_id");
             $table->integer("account_number");
             $table->timestamps();
-            $table->unique(['contact_id','action_type','recordable_at']);
+            $table->unique(['contact_id','action_type','recorded_at']);
 
         });
     }
