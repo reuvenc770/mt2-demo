74b1fe0e2b277d28e218ab2c85508d2910076ab8
rbertorelli <rbertorelli@zetainteractive.com>
2017-02-03 21:01:42 +0000

    New models and migrations - replacements for record_data

---
+++ app/Models/EmailFeedAttributableData.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmailFeedAttributableData extends Model
+{
+    protected $table = 'email_feed_attributable_data';
+    protected $guarded = [];
+}
