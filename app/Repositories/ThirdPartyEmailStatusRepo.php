2129de461119714995bef0e80a9065f4cc548804
rbertorelli <rbertorelli@zetainteractive.com>
2017-02-08 20:59:32 +0000

    New repo for email status (will also include stuff for last action

---
+++ app/Repositories/ThirdPartyEmailStatusRepo.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\ThirdPartyEmailStatus;
+use App\Repositories\RepoTraits\Batchable;
+
+class ThirdPartyEmailStatusRepo {
+    use Batchable;
+
+    private $model;
+
+    public function __construct(ThirdPartyEmailStatus $model) {
+        $this->model = $model;
+    }
+
+    public function getActionStatus($emailId) {
+        $row = $this->model->where('email_id', $emailId);
+
+        if ($row) {
+            return $row->last_action_type;
+        }
+        else {
+            return null;
+        }
+    }
+
+    private function buildBatchedQuery($data) {
+        DB::statement("INSERT INTO email_feed_record_data (email_id, last_action_type,
+            last_action_offer_id, last_action_datetime, last_action_esp_account_id,
+            created_at, updated_at)
+
+            VALUES
+
+            $data
+
+            ON DUPLICATE KEY UPDATE
+            email_id = email_id,
+            last_action_type = values(last_action_type),
+            last_action_datetime = values(last_action_datetime),
+            last_action_esp_account_id = values(last_action_esp_account_id),
+            created_at = created_at,
+            updated_at = values(updated_at)");
+    }
+
+    private function transformRowToString($row) {
+        $pdo = DB::connection()->getPdo();
+
+        return '(' . 
+            . $pdo->quote($row['email_id']) . ','
+            . $pdo->quote($row['last_action_type']) . ','
+            . $pdo->quote($row['last_action_offer_id']) . ','
+            . $pdo->quote($row['last_action_datetime']) . ','
+            . $pdo->quote($row['last_action_esp_account_id']) . 'NOW() , NOW())';
+    }
+}
