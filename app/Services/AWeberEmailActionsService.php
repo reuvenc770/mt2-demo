c499cc172da095d0f75abed85f2a11c822cf971a
Patrick Cunningham <pat@codedestroyer.com>
2017-02-17 11:35:10 -0500

    missing method

--- app/Services/AWeberEmailActionsService.php
+++ app/Services/AWeberEmailActionsService.php
@@ -69,5 +69,11 @@ class AWeberEmailActionsService
       return $this->subRepo->getByInternalId($internalId);
     }
 
+    public function getEmailAddressById($id){
+        return $this->subRepo->getByInternalId($id)->email_address;
+    }
+
+
+
     
 }
