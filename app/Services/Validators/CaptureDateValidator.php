538241b1f0a345875a1664cf50c9d23cbef122d9
rbertorelli <rbertorelli@zetainteractive.com>
2017-03-09 21:58:38 +0000

    Cleanup and additional notes (for possible future expansion in logic) on capture date validator

--- app/Services/Validators/CaptureDateValidator.php
+++ app/Services/Validators/CaptureDateValidator.php
@@ -28,14 +28,18 @@ class CaptureDateValidator implements IValidate {
             $parsedDate = Carbon::parse($this->captureDate);
 
             // The system doesn't handle capture dates in the future too well.
-            if ($parsedDate->gt($today) || '0000-00-00' === $parsedDate->format('Y-m-d')) {
+            if ($parsedDate->gt($today) || '0000-00-00' === $this->captureDate) {
                 $this->captureDate = $today->format('Y-m-d');
             }
         }
         catch (Exception $e) {
             // Carbon throws this is it can't parse the date.
             // In that case, throw validation error, as per Ken's request
-            throw new ValidationException("Capture date '{$this->captureDate}'' cannot be parsed");
+
+            // According to Jim, the date formats currently received look like
+            // yyyy-mm-dd hh:mm:ss or mm/dd/yyyy hh:mm:ss, but we should not rule out other types 
+            // and should check the errors logs often to find new parsable formats.
+            throw new ValidationException("Capture date '{$this->captureDate}' cannot be parsed");
         }
         
     }
