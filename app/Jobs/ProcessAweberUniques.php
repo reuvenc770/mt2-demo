1a8f66e3990ba1afd1e02255ec274f3581daca13
Patrick Cunningham <codedestroyer@Patricks-MBP.home>
2017-03-13 16:09:16 -0400

    added rate laning

--- app/Jobs/ProcessAweberUniques.php
+++ app/Jobs/ProcessAweberUniques.php
@@ -8,16 +8,17 @@ use App\Models\AWeberReport;
 use App\Models\JobEntry;
 use App\Factories\APIFactory;
 use Illuminate\Queue\SerializesModels;
+use App\Facades\JobTracking;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use App\Jobs\Traits\PreventJobOverlapping;
-use App\Facades\JobTracking;
-use Log;
+use Carbon\Carbon;
+use Cache;
 class ProcessAweberUniques extends Job implements ShouldQueue
 {
     use InteractsWithQueue, SerializesModels;
     
     protected $jobName = 'ProcessAweberUniques-';
+    CONST KEY_NAME = "AWEBER_RATE_LIMIT_";
     protected $tracking;
     protected $id;
     protected $espAccountId;
@@ -49,21 +50,31 @@ class ProcessAweberUniques extends Job implements ShouldQueue
      */
     public function handle()
     {
+        if(Cache::get(self::KEY_NAME.$this->espAccountId) == 1){
+            $this->release(0);
+        };
+
+        $time = Carbon::now()->addMinutes(120);
+        Cache::put(self::KEY_NAME.$this->espAccountId,1,$time);//lock this account
+
         $reportService = APIFactory::createAPIReportService("AWeber", $this->espAccountId);
         try {
             JobTracking::changeJobState(JobEntry::RUNNING, $this->tracking);
             switch ($this->type) {
                 case AWeberReport::UNIQUE_OPENS:
                     $value = $reportService->getUniqueStatForCampaignUrl($this->infoUrl, AWeberReport::UNIQUE_OPENS);
+                    Cache::forget(self::KEY_NAME.$this->espAccountId);//give up the key
                     $reportService->updateUniqueStatForCampaignUrl($this->id, $this->type, $value);
                     break;
                 case AWeberReport::UNIQUE_CLICKS:
                     $value = $reportService->getUniqueStatForCampaignUrl($this->infoUrl, AWeberReport::UNIQUE_CLICKS);
+                    Cache::forget(self::KEY_NAME.$this->espAccountId);//give up the key
                     $reportService->updateUniqueStatForCampaignUrl($this->id, $this->type, $value);
                     break;
                 default:
                     throw new JobException("Not a valid action type");
             }
+
             JobTracking::changeJobState(JobEntry::SUCCESS, $this->tracking);
         } catch (\Exception $e) {
             throw new JobException("{$this->jobName} failed with {$e->getMessage()}  {$e->getLine()}" . PHP_EOL);
@@ -71,6 +82,7 @@ class ProcessAweberUniques extends Job implements ShouldQueue
     }
 
     public function failed() {
+        Cache::forget(self::KEY_NAME.$this->espAccountId);
         JobTracking::changeJobState(JobEntry::FAILED,$this->tracking);
     }
 }
