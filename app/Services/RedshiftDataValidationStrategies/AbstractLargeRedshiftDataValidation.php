acf32a2a775f0bfa1e744aebd41a158df88159ad
rbertorelli <rbertorelli@zetainteractive.com>
2017-03-14 20:06:32 +0000

    AbstractLargeRedshiftDataValidation.php edited online with Bitbucket - fixing major error with rate - were using the % inverse of what we wanted to measure.

--- app/Services/RedshiftDataValidationStrategies/AbstractLargeRedshiftDataValidation.php
+++ app/Services/RedshiftDataValidationStrategies/AbstractLargeRedshiftDataValidation.php
@@ -8,7 +8,7 @@ abstract class AbstractLargeRedshiftDataValidation {
 
     protected $cmpRepo;
     protected $redshiftRepo;
-    const ACCEPTABLE_DIFF_RATE = 0.0001;
+    const ACCEPTABLE_ERROR_RATE = 0.0001;
     const TEST_COUNT = 10000;
     const PASSING_T_SCORE = 31.82; // derived from table
 
@@ -69,7 +69,7 @@ abstract class AbstractLargeRedshiftDataValidation {
         
         // sn = sqrt((np(1-p)) / (n - 1)) - it's a proportion, not a number
         $sampleStdDev = sqrt((self::TEST_COUNT * ($matches / self::TEST_COUNT) * ((self::TEST_COUNT - $matches) / self::TEST_COUNT)) / (self::TEST_COUNT - 1));
-        $tScore = abs(($matches / self::TEST_COUNT) - self::ACCEPTABLE_DIFF_RATE) / ($sampleStdDev / sqrt(self::TEST_COUNT));
+        $tScore = abs((1 - ($matches / self::TEST_COUNT)) - self::ACCEPTABLE_ERROR_RATE) / ($sampleStdDev / sqrt(self::TEST_COUNT));
         
         Log::info("$entity has $matches matches out of " . self::TEST_COUNT . " for a t-score of $tScore against " . self::PASSING_T_SCORE . '.');
         Log::info("$entity took $testTime seconds. $redshiftTime for redshift and $cmpTime for CMP db.");
