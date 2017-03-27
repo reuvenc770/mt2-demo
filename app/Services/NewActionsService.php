c14dbd83032acda3cc17a15017d0e9b7a5ada0d5
rbertorelli <rbertorelli@zetainteractive.com>
2017-03-27 21:17:45 +0000

    Adding first party updating to new actions job

--- app/Services/NewActionsService.php
+++ app/Services/NewActionsService.php
@@ -8,16 +8,23 @@ namespace App\Services;
 use App\Repositories\ListProfileFlatTableRepo;
 use App\Repositories\ThirdPartyEmailStatusRepo;
 use App\Repositories\AttributionRecordTruthRepo;
+use App\Repositories\FirstPartyRecordDataRepo;
 
 class NewActionsService {
     protected $lpFlatRepo;
     protected $emailStatusRepo;
     protected $recordTruthRepo;
+    private $firstPartyRepo;
 
-    public function __construct ( ListProfileFlatTableRepo $lpFlatRepo , ThirdPartyEmailStatusRepo $emailStatusRepo , AttributionRecordTruthRepo $recordTruthRepo ) {
+    public function __construct ( ListProfileFlatTableRepo $lpFlatRepo , 
+        ThirdPartyEmailStatusRepo $emailStatusRepo , 
+        AttributionRecordTruthRepo $recordTruthRepo,
+        FirstPartyRecordDataRepo $firstPartyRepo) {
+
         $this->lpFlatRepo = $lpFlatRepo;
         $this->emailStatusRepo = $emailStatusRepo;
         $this->recordTruthRepo = $recordTruthRepo;
+        $this->firstPartyRepo = $firstPartyRepo;
     }
 
     public function updateThirdPartyEmailStatuses ( $dateRange ) {
@@ -26,6 +33,22 @@ class NewActionsService {
 
     public function updateAttributionRecordTruths ( $dateRange ) {
         $this->runUpdateFromListProfileFlatTable( 'recordTruthRepo' , 'getRecordTruthsExtractQuery' , $dateRange );
+    }
+
+    public function updateFirstPartyEmailStatuses($dateRange) {
+        $pdo = \DB::connection($this->lpFlatRepo->getConnection())->getPdo();
+        $pdo->setAttribute(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+
+        $query = $this->lpFlatRepo->getFirstPartyActionStatusQuery();
+        $statement = $pdo->prepare($query);
+        $statement->execute([':start' => $dateRange['start'], ':end' => $dateRange['end']]);
+
+        while($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
+            $mappedRow = $this->mapBetweenActionAndFirstParty($row);
+            $this->firstPartyRepo->updateActionData($mappedRow);
+        }
+
+        $this->firstPartyRepo->cleanUpActions();
     }
 
     protected function runUpdateFromListProfileFlatTable ( $repoName , $extractMethod , $dateRange ) {
@@ -44,5 +67,16 @@ class NewActionsService {
         }
 
         $this->$repoName->insertStored();
+    }
+
+    private function mapBetweenActionAndFirstParty($row) {
+        return [
+            'email_id' => $row[''],
+            'feed_id' => $row[''],
+            'last_action_date' => $row[''],
+            'last_action_offer_id' => $row[''],
+            'last_action_esp_account_id' => $row[''],
+            'last_action_type_id' => $row[''],
+        ];
     }
 }
