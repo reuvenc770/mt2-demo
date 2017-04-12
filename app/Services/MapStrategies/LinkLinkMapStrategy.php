3ebc7f7cba2b07352247ed114e323949febc906f
rbertorelli <rbertorelli@zetainteractive.com>
2017-04-12 16:53:27 +0000

    Fixing process in link sync that was missing mt2-generated links

--- app/Services/MapStrategies/LinkLinkMapStrategy.php
+++ app/Services/MapStrategies/LinkLinkMapStrategy.php
@@ -3,14 +3,18 @@
 namespace App\Services\MapStrategies;
 
 use App\Services\Interfaces\IMapStrategy;
+use Carbon\Carbon;
 
 class LinkLinkMapStrategy implements IMapStrategy {
 
     public function map($record) {
+        $createDatetime = $record['date_added'] === '0000-00-00 00:00:00' ? 
+            Carbon::now()->toDateTimeString() : $record['date_added'];
+
         return [
             'id' => $record['link_id'],
             'url' => $record['refurl'],
-            'created_at' => $record['date_added']
+            'created_at' => $createDatetime
         ];
     }
 }
