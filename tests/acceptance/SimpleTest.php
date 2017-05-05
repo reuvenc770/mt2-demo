10125bddeee9f0e1f32d28410e2e3789833e68c6
Adam Chin <achin@zetainteractive.com>
2017-05-05 11:02:18 -0400

    Adding namespace to SimpleTest.

--- tests/acceptance/SimpleTest.php
+++ tests/acceptance/SimpleTest.php
@@ -1,11 +1,12 @@
 <?php
 
+namespace Tests\Acceptance;
+
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 use Tests\TestCase;
-
 
 class SimpleTest extends TestCase
 {
