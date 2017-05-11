ed6fe80923f9119f1242543f174f21470d71425c
Adam Chin <achin@zetainteractive.com>
2017-05-11 16:38:45 -0400

    Committing working single email for missing mapping w/ a template.

---
+++ resources/views/emails/feedNoMappingAlert.blade.php
@@ -0,0 +1,72 @@
+@extends( 'emails.cmpTemplate' )
+
+@section( 'primaryHeader' )
+Alerts
+@endsection
+
+@section( 'secondaryHeader' )
+Feed Processing - Missing Column Mapping
+@endsection
+
+@section( 'content' )
+                <table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                  <tbody>
+                    <tr>
+                      <td style="word-wrap:break-word;font-size:0px;padding:10px 25px;" align="left">
+                        <div class="" style="cursor:auto;color:#000000;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:22px;text-align:left;">The following clients do not have column mapping defined for processing but recently uploaded new record files.</div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td style="word-wrap:break-word;font-size:0px;padding:10px 25px;" align="left">
+                        <table cellpadding="0" cellspacing="0" style="cellspacing:0px;color:#000;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:22px;table-layout:auto;" width="100%" border="0">
+                          <tr style="border-bottom:1px solid #ecedee;text-align:left;padding:15px 0;">
+                            <th>Feed</th>
+                            <th>Files</th>
+                          </tr>
+                        
+@foreach ( $fileList as $feedName => $files )
+    {{ $first = true }}
+    @foreach ( $files as $file )
+        <tr>
+            <td>
+                @if ( $first )
+                    {{ $feedName }}
+                @endif
+            </td>
+            <td>{{ $file }}</td>
+        </tr>
+        {{ $first = false }}
+    @endforeach 
+@endforeach
+<!--
+                          <tr>
+                            <td>FD1</td>
+                            <td>/home/FD1/upload/notification_test_file11.csv</td>
+                          </tr>
+                          <tr>
+                            <td></td>
+                            <td>/home/FD1/upload/notification_test_file12.csv</td>
+                          </tr>
+                          <tr>
+                            <td>FD2</td>
+                            <td>/home/FD2/upload/notification_test_file13.csv</td>
+                          </tr>
+                          <tr>
+                            <td></td>
+                            <td>/home/FD2/upload/notification_test_file14.csv</td>
+                          </tr>
+                          <tr>
+                            <td></td>
+                            <td>/home/FD2/upload/notification_test_file15.csv</td>
+                          </tr>
+                          <tr>
+                            <td></td>
+                            <td>/home/FD2/upload/notification_test_file16.csv</td>
+                          </tr>
+-->
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+@endsection
