f116ad3c484ab21ec72e121681d9435cbf86daf8
Adam Chin <achin@zetainteractive.com>
2017-06-01 18:39:31 -0400

    Committing models and migrations.

---
+++ database/migrations/2017_06_01_142710_create_notification_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notification_logs', function (Blueprint $table) {
+            $table->string( 'content_key' );
+            $table->text( 'content' );
+
+            $table->timestamp( 'created_at' )->default( DB::raw('CURRENT_TIMESTAMP') );
+
+            $table->index( 'content_key' );
+            $table->index( 'created_at' );
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('notification_logs');
+    }
+}
