06b0383aedf9918ba880e1a7c82315a90eef2016
rbertorelli <rbertorelli@zetainteractive.com>
2017-06-12 17:16:08 +0000

    ListProfileQueryBuilder.php edited online with Bitbucket

--- app/Builders/ListProfileQueryBuilder.php
+++ app/Builders/ListProfileQueryBuilder.php
@@ -73,7 +73,7 @@ class ListProfileQueryBuilder {
             'device_name' => 'rd.device_name',
             'carrier' => 'rd.carrier',
             'action_status' => 'rd.last_action_type',
-            'action_date' => 'rd.last_action_date'
+            'action_date' => 'rd.last_action_date',
             'globally_suppressed' => DB::connection('redshift')->raw("(s.email_address IS NOT NULL) AS globally_suppressed"),
             'feed_suppressed' => DB::connection('redshift')->raw("(sls.email_address IS NOT NULL) AS feed_suppressed"),
         ];
