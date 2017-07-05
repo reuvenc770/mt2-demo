c747dab8a0010662e0465ec24d114426fa137ffc
Adam Chin <achin@zetainteractive.com>
2017-07-05 17:07:42 -0400

    Committing rerun command adjustment for realtime feeds and moving files to archive after they are done.

--- app/Jobs/CMPTE/RealtimeProcessingJob.php
+++ app/Jobs/CMPTE/RealtimeProcessingJob.php
@@ -9,6 +9,7 @@ use App\Jobs\ProcessFeedRawFilesJob;
 use App\Services\RemoteFeedFileService;
 
 class RealtimeProcessingJob extends ProcessFeedRawFilesJob {
+    const ARCHIVE_DIR = '/var/local/programdata/done/mt2_realtime_archive/';
     protected $jobName = 'RealtimeProcessingJob-';
 
     public function __construct ( $tracking ) {
@@ -18,14 +19,13 @@ class RealtimeProcessingJob extends ProcessFeedRawFilesJob {
     public function handle ( RemoteFeedFileService $service ) {
         $service = \App::make( \App\Services\CMPTE\RealtimeProcessingService::class );
 
-        /**
-         *
-         * Uncomment this when we're ready for CMPTE 
-         *
         $service->setFileProcessedCallback( function ( $file , $systemService , $meta ) {
-            $systemService->deleteFile( $file[ 'path' ] );
+            #only do this for normal realtime since there are so many more records and seems like mt1 overwrites these files.
+            if ( strpos( $file[ 'path' ] , 'mt2_realtime' ) !== false ) {
+                $newPath = self::ARCHIVE_DIR . basename( $file[ 'path' ] );
+                $output = $systemService->moveFile( $file[ 'path' ] , $newPath );
+            }
         } );
-         */
 
         parent::handle( $service );
     }
