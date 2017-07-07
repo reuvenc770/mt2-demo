64ca62f8d25b9a3bf42bccd1a83b463ab75667b0
Adam Chin <achin@zetainteractive.com>
2017-07-07 10:55:30 -0400

    Adding check for unprocessed realtime files, template and processed file service.

--- app/Console/Commands/CheckBatchFeedProcessingCommand.php
+++ app/Console/Commands/CheckBatchFeedProcessingCommand.php
@@ -5,6 +5,8 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\MT1Models\User as Feeds;
 use Maknz\Slack\Facades\Slack;
+use Carbon\Carbon;
+use Notify;
 
 class CheckBatchFeedProcessingCommand extends Command
 {
@@ -24,6 +26,7 @@ class CheckBatchFeedProcessingCommand extends Command
 
     const SLACK_CHANNEL = '#cmp_hard_start_errors';
     protected $remote;
+    protected $feedFileService;
 
     /**
      * Create a new command instance.
@@ -43,6 +46,7 @@ class CheckBatchFeedProcessingCommand extends Command
     public function handle()
     {
         $this->remote = \App::make( \App\Services\RemoteLinuxSystemService::class );
+        $this->feedFileService = \App::make( \App\Services\ProcessedFeedFileService::class ); 
 
         $this->remote->initSshConnection(
             config('ssh.servers.cmpte_feed_file_server.host'),
@@ -57,6 +61,19 @@ class CheckBatchFeedProcessingCommand extends Command
         $this->alertForOrangeFiles();
 
         $this->moveRedFiles();
+
+        /**
+         * Switching connection to realtime for other checks
+         */
+        $this->remote->initSshConnection(
+            config('ssh.servers.cmpte_realtime_server.host'),
+            config('ssh.servers.cmpte_realtime_server.port'),
+            config('ssh.servers.cmpte_realtime_server.username'),
+            config('ssh.servers.cmpte_realtime_server.public_key'),
+            config('ssh.servers.cmpte_realtime_server.private_key')
+        );
+
+        $this->findUnprocessedRealtimeFiles();
     }
 
     protected function createNewFeedMt1Directories () {
@@ -124,6 +141,60 @@ class CheckBatchFeedProcessingCommand extends Command
                         $output = $this->remote->moveFile( $redFile , $newPath );
                     }
                 }
+            }
+        }
+    }
+
+    protected function findUnprocessedRealtimeFiles () {
+        $realtimeDirectory = '/var/local/programdata/done/mt2_realtime/';
+
+        $findOptions = [
+            '-type f' ,
+            '-mtime -1' ,
+            '-printf "%f,%TY-%Tm-%Td %TH:%Tm\n"'
+        ];
+
+        if ( $newFileString = $this->remote->getRecentFiles( $realtimeDirectory , $findOptions ) ) {
+            $files = [];
+            $unprocessedFiles = [];
+
+            foreach ( explode( "\n" , $newFileString ) as $line ) {
+                if ( empty( $line ) ) {
+                    continue;
+                }
+
+                $lineArray = explode( ',' , $line );
+
+                $file = [
+                    'path' => $realtimeDirectory . $lineArray[ 0 ] ,
+                    'count' => $this->remote->getFileLineCount( $realtimeDirectory . $lineArray[ 0 ] ) ,
+                    'lastModified' => Carbon::parse( $lineArray[ 1 ] )
+                ];
+
+                $files []= $file;
+
+                if ( !$this->feedFileService->fileWasProcessed( $file[ 'path' ] ) ) {
+                    $unprocessedFiles []= array_merge( $file , [ 'reason' => 'Could not find file in processed_feed_files table.' ] );
+
+                    continue;
+                }
+
+                if ( !$this->feedFileService->fileLineCountMatches( $file[ 'path' ] , $file[ 'count' ] ) ) {
+                    $unprocessedFiles []= array_merge( $file , [ 'reason' => 'Line Count did not match in processed_feed_files table.' ] );
+
+                    continue;
+                }
+
+                $timestamp = $this->feedFileService->getProcessedTime( $file[ 'path' ] );
+                if ( -60 > Carbon::parse( $file[ 'lastModified' ] )->diffInMinutes( $timestamp , false ) ) {
+                    $unprocessedFiles []= array_merge( $file , [ 'reason' => 'File last modified is more than an hour past it\'s processed time.' ] );
+
+                    continue;
+                }
+            }
+
+            if ( count( $unprocessedFiles ) ) {
+                Notify::log( 'realtime_feed_file_unprocessed' , json_encode( [ "files" => $unprocessedFiles ] ) );
             }
         }
     }
