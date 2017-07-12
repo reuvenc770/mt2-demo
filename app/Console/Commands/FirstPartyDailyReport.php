01b02296ee0cb047448c94b1a8ada486deca9c05
rbertorelli <rbertorelli@zetaglobal.com>
2017-07-12 22:04:24 +0000

    WIP switching to another branch (hence the notes)

--- app/Console/Commands/FirstPartyDailyReport.php
+++ app/Console/Commands/FirstPartyDailyReport.php
@@ -4,7 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Repositories\EspWorkflowRepo;
-use App\Repositories\FeedDateEmailBreakdownRepo;
+use App\Repositories\EspWorkflowLogRepo;
+use Carbon\Carbon;
 
 class FirstPartyDailyReport extends Command
 {
@@ -36,16 +37,64 @@ class FirstPartyDailyReport extends Command
      *
      * @return mixed
      */
-    public function handle(EspWorkflowRepo $repo, FeedDateEmailBreakdownRepo $statRepo) {
+    public function handle(EspWorkflowRepo $repo, EspWorkflowLogRepo $logRepo) {
         // Build out something like a report card ... need to get all active 1st party feeds
 
-        $firstPartyFeeds = $workflowRepo->getActiveWorkflows();
+        $yesterday = Carbon::yesterday()->toDateString();
+        $lists = $logRepo->getActiveLists($yesterday);
 
-        foreach($firstPartyFeeds as $feed) {
+        // need lists here instead
 
-            $data = $statRepo->
+        foreach($lists as $list) {
+            $data = $logRepo->getDataForDate($list->target_list, $yesterday);
+            $mtdCount = $logRepo->monthToDateCount($list->target_list, $yesterday);
 
-            Notify::sendData($data);
+            // So, how to handle when the target list belongs to a group?
+            // Is this why we had workflow earlier?
+
+            // Different data streams can use the same workflow
+            // 
+            So is it for each workflow for each ... stream?
+
+            No.
+
+            There are many streams. Many can belong to a workflow.
+
+            So yes, this is for each workflow for each stream.
+            What does a stream consist of?
+            - BelongTo a workflow
+            - 
         }
+
+        Notify::sendData($data);
     }
 }
+
+
+        // 1. total count for date
+        // 2. 
+        /*
+Foodstamps Count
+Users Received from Foodstamps: 3450
+Users Sent To Foodstamps List - 3413
+Dupe - 23
+Total Duplicates who have signed up 3X or More - 1
+Users Received Month to Date from Foodstamps: 71121
+---------------------------------------------------
+HealthPlansOfAmerica Counts
+Users Received from 0bce03ec00000000000000000000000fe75d: 416
+Users Received from 0bce03ec000000000000000000000010cb84: 157
+Users Received from 0bce03ec000000000000000000000010cb85: 262
+Users Sent To Bronto by Status
+    0bce03ec00000000000000000000000fe75d - Created - 353
+    0bce03ec00000000000000000000000fe75d - Duplicate - 63
+    0bce03ec000000000000000000000010cb84 - Created - 132
+    0bce03ec000000000000000000000010cb84 - Duplicate - 25
+    0bce03ec000000000000000000000010cb85 - Created - 209
+    0bce03ec000000000000000000000010cb85 - Duplicate - 52
+    0bce03ec000000000000000000000010cb85 - Other Error - 1
+Users Received Month To Date from 0bce03ec00000000000000000000000fe75d: 23559
+Users Received Month To Date from 0bce03ec000000000000000000000010cb84: 9818
+Users Received Month To Date from 0bce03ec000000000000000000000010cb85: 10070
+
+        */
--- app/Repositories/EspWorkflowLogRepo.php
+++ app/Repositories/EspWorkflowLogRepo.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Models\EspWorkflowLog;
 use Carbon\Carbon;
+use DB;
 
 class EspWorkflowLogRepo {
     
@@ -14,38 +15,33 @@ class EspWorkflowLogRepo {
     }
 
     public function getDataForDate($list, $date) {
-        // 1. total count for date
-        // 2. 
-        /*
-Foodstamps Count
-Users Received from Foodstamps: 3450
-Users Sent To Foodstamps List - 3413
-Dupe - 23
-Total Duplicates who have signed up 3X or More - 1
-Users Received Month to Date from Foodstamps: 71121
----------------------------------------------------
-HealthPlansOfAmerica Counts
-Users Received from 0bce03ec00000000000000000000000fe75d: 416
-Users Received from 0bce03ec000000000000000000000010cb84: 157
-Users Received from 0bce03ec000000000000000000000010cb85: 262
-Users Sent To Bronto by Status
-    0bce03ec00000000000000000000000fe75d - Created - 353
-    0bce03ec00000000000000000000000fe75d - Duplicate - 63
-    0bce03ec000000000000000000000010cb84 - Created - 132
-    0bce03ec000000000000000000000010cb84 - Duplicate - 25
-    0bce03ec000000000000000000000010cb85 - Created - 209
-    0bce03ec000000000000000000000010cb85 - Duplicate - 52
-    0bce03ec000000000000000000000010cb85 - Other Error - 1
-Users Received Month To Date from 0bce03ec00000000000000000000000fe75d: 23559
-Users Received Month To Date from 0bce03ec000000000000000000000010cb84: 9818
-Users Received Month To Date from 0bce03ec000000000000000000000010cb85: 10070
+        $start = $date . ' 00:00:00';
+        $end = $date . ' 23:59:59';
 
-        */
-        return $this->model->
+        return $this->model
+                    ->select(DB::raw("SUM(IF(times_sent > 1, 1, 0)) as duplicates"), 
+                            DB::raw("SUM(IF(times_sent > 1, 1, 0)) as egregious_duplicates"))
+                    ->where('target_list', $list)
+                    ->whereBetween('created_at', [$start, $end])
+                    ->get();
     }
 
-    public function monthToDateCount($list) {
-        $monthStart = Carbon::today()->format('Y-m-01 00:00:00');
-        return $this->model->where('target_list', $list)->where('created_at', '>=', $monthStart)->count();
+    public function monthToDateCount($list, $date) {
+        $monthStart = Carbon::parse($date)->format('Y-m-01 00:00:00');
+        return $this->model
+                    ->where('target_list', $list)
+                    ->whereBetween('created_at', [$monthStart, $date])
+                    ->count();
+    }
+
+    public function getUnsubscribe($list, $date) {}
+
+    public function getActiveLists($date) {
+        $start = $date . ' 00:00:00';
+        $end = $date . ' 23:59:59';
+        return $this->model
+                    ->selectRaw("distinct target_list")
+                    ->whereBetween('created_at', [$start, $end])
+                    ->get();
     }
 }
