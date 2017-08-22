6c787a7534258222cdbab05783dac1a23d8f04d1
rbertorelli <rbertorelli@zetainteractive.com>
2017-08-22 16:16:13 +0000

    EmailFeedInstanceRepo.php edited online with Bitbucket - trying to find a way to remove null

--- app/Repositories/EmailFeedInstanceRepo.php
+++ app/Repositories/EmailFeedInstanceRepo.php
@@ -59,6 +59,7 @@ class EmailFeedInstanceRepo implements ICanonicalDataSource {
 
     private function transformRowToString($row) {
         $pdo = DB::connection()->getPdo();
+        $dob = is_null($row['dob']) || ('0000-00-00' === $row['dob']) ? 'NULL' : $pdo->quote($row['dob']);
 
         return '('
             . $pdo->quote($row['email_id']) . ','
@@ -74,7 +75,7 @@ class EmailFeedInstanceRepo implements ICanonicalDataSource {
             . $pdo->quote($row['state']) . ','
             . $pdo->quote($row['zip']) . ','
             . $pdo->quote($row['country']) . ','
-            . $pdo->quote($row['dob']) . ','
+            . $dob . ','
             . $pdo->quote($row['gender']) . ','
             . $pdo->quote($row['phone']) . ','
             . $pdo->quote($row['mobile_phone']) . ','
