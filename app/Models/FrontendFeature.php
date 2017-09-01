d4597ae9e97b2fe6c70b45fe6f9918f6e11cda0d
Adam Chin <achin@zetainteractive.com>
2017-09-01 10:45:45 -0400

    Committing schema and mapping.

---
+++ app/Models/FrontendFeature.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FrontendFeature extends Model
+{
+    protected $guareded = [];
+
+    public function pages() {
+        return $this->hasMany('App\Models\Page');
+    }
+
+    public function permissions() {
+        return $this->belongsToMany('App\Models\Permission', 'frontend_feature_permission_mappings', 'frontend_feature_id', 'permission_id');
+    }
+}
