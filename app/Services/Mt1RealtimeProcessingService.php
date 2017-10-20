68d9f647b8ef47e1e6febac2c1b371a6a98b1f96
admchin <achin@zetainteractive.com>
2017-10-20 22:01:43 +0000

    Mt1RealtimeProcessingService.php edited online with Bitbucket

--- app/Services/Mt1RealtimeProcessingService.php
+++ app/Services/Mt1RealtimeProcessingService.php
@@ -152,6 +152,8 @@ class Mt1RealtimeProcessingService extends RemoteFeedFileService {
             $record[ 'source_url' ] = $this->feedService->getSourceUrlFromId( $record[ 'feed_id' ] );
         }
 
+        $record[ 'source_url' ] = ( strlen( $record[ 'source_url' ] ) > 255 ) ? substr( $record[ 'source_url' ] , 0 , 254 ) : $record[ 'source_url' ];
+
         if ( isset( $record[ 'dob' ] ) && $record[ 'dob' ] == '0000-00-00' ) {
             unset( $record[ 'dob' ] );
         } 
