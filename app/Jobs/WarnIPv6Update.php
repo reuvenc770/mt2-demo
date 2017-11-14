1e881ca65ca26f0e3f0cb73ea39de85369261318
rbertorelli <rbertorelli@zetainteractive.com>
2017-11-14 21:32:54 +0000

    WarnIPv6Update.php edited online with Bitbucket - fixing two small bugs in the warn ipv6 definitions job

--- app/Jobs/WarnIPv6Update.php
+++ app/Jobs/WarnIPv6Update.php
@@ -13,11 +13,11 @@ class WarnIPv6Update extends MonitoredJob {
     }
 
     protected function handleJob() {
-        $repo = App::make(\App\Repositories\Ipv6CountryMappingRepo::class);
+        $repo = \App::make(\App\Repositories\Ipv6CountryMappingRepo::class);
         $updateDay = $repo->getLastUpdate();
 
         // We want to update this once a month or so
-        if (Carbon::today()->gt(Carbon::parse($updateDate)->addDays(30))) {
+        if (Carbon::today()->gt(Carbon::parse($updateDay)->addDays(30))) {
             Mail::raw("IPv6 data last updated at $updateDay. Please download ipv6 to country mapping file, please in storage/app directory, and run UploadIPv6DB. See expected file format in UploadIPv6DBJob. MaxMind GeoIPv6 Country should match this format.", function ($message) {
                 $message->to(config('contacts.tech'));
                 $message->subject("Alert! IPv6 definitions may be out of date.");
