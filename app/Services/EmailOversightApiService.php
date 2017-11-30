ad47e5e3cd3f3c3ab264d7386b54b8b951f36d8c
Adam Chin <achin@zetainteractive.com>
2017-11-30 17:54:04 -0500

    Removing unneeded code.

--- app/Services/EmailOversightApiService.php
+++ app/Services/EmailOversightApiService.php
@@ -12,19 +12,6 @@ class EmailOversightApiService {
     protected $cache;
 
     protected $validCodes = [ 1 , 11 ];
-    protected $invalidCodeSuppTypeMap = [
-        2 => 56 ,
-        3 => 57 ,
-        4 => 58 ,
-        5 => 59 ,
-        6 => 60 ,
-        7 => 61 ,
-        9 => 62 ,
-        10 => 63 ,
-        13 => 64 ,
-        20 => 65
-    ];
-
     protected $lastResultMessage;
 
     public function __construct ( EmailOversightApi $api , EmailOversightValidCacheRepo $cache ) {
@@ -61,11 +48,7 @@ class EmailOversightApiService {
 
     protected function suppressEmail ( $email , $validationResponse ) {
         $date = Carbon::today()->toDateString();
-        $reasonId = 0;
-
-        if ( isset( $this->invalidCodeSuppTypeMap[ $validationResponse->ResultId ] ) ) {
-            $reasonId = Suppression::getReasonIdFromSubstring( 'Oversight - ' . $validationResponse->Result );
-        }
+        $reasonId = Suppression::getReasonIdFromSubstring( 'Oversight - ' . $validationResponse->Result );
 
         Suppression::recordSuppressionByReason( $email , $date , $reasonId );
         Suppression::recordGlobalSuppressionByReason( $email , $date , $reasonId );
