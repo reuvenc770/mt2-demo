7c1fcb1e91e15cd345e1d1bbe76f5f3b36c81153
Melissa Lin <mlin@zetainteractive.com>
2017-12-07 10:50:35 -0500

    started user acceptance tests

--- tests/acceptance/UserRolesCest.php
+++ tests/acceptance/UserRolesCest.php
@@ -9,14 +9,34 @@ class UserRolesCest
         $I->wantTo('successfully log in');
 
         $this->loginUser( $I , 'admin@mt2.com' , 'admin' , 'Admin McAdmin' );
+        $I->see( 'Admin McAdmin' );
+        $I->seeInCurrentUrl( '/home' );
+    }
+
+    public function tryToViewProfile(AcceptanceTester $I) {
+        $I->wantTo('view profile page');
+
+        $this->tryToSuccessfullyLogIn($I);
+
+        $I->click('Admin McAdmin');
+        $I->amOnPage( '/myprofile' );
+    }
+
+    public function tryToCreateNewClient(AcceptanceTester $I) {
+        $I->wantTo('create a new client record');
+
+        $this->tryToSuccessfullyLogIn($I);
+
+        $I->click('Clients', '#mainSideNav');
+        $I->amOnPage('/client');
+        $I->click('Add Client');
+        $I->amOnPage('/client/create');
     }
 
     private function loginUser( AcceptanceTester $I , $email , $password, $name ) {
         $I->amOnPage('/login');
-        $I->fillField( 'login' , $email );
+        $I->fillField(['name' => 'login'], $email );
         $I->fillField( 'password' , $password );
         $I->click('Login' , '.btn');
-        $I->see( $name );
-        $I->seeInCurrentUrl( '/home' );
     }
 }
