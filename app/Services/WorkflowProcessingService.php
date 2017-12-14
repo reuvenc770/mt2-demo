210eac63b192ca7d751a348cad50acb7317d386f
rbertorelli <rbertorelli@zetaglobal.com>
2017-12-14 00:19:15 +0000

    Redid first party reprocessing to be by-feed

--- app/Services/WorkflowProcessingService.php
+++ app/Services/WorkflowProcessingService.php
@@ -32,9 +32,7 @@ class WorkflowProcessingService {
 
         $deployIds = $this->stepsRepo->getDeployIds($workflow->id);
         $offerIds = $this->stepsRepo->getOfferIds($workflow->id);
-        
-        $suppressionLists = $this->getSuppressionListIds($workflow);
-        $this->processingStrategy->setSuppressionListIds($suppressionLists);
+        $this->processingStrategy->setTargetId($workflow->esp_suppression_list);
 
         $resource = $this->actionsRepo->getEmailsForDeploys($deployIds, $daysBack);
 
@@ -42,22 +40,14 @@ class WorkflowProcessingService {
             // Run these against all suppression lists
             foreach ($offerIds as $offerId) {
                 if ($this->suppService->isSuppressed($emailAddress, $offerId)) {
-                    // If suppressed, upload to all lists
+                    // If suppressed, upload to specified list
+                    $listId = $this->suppService->getAdvertiserList($offerId);
+                    $this->processingStrategy->setTargets([$listId]);
                     $this->processingStrategy->processSuppression($emailAddress);
-                    break;
                 }
             }                    
 
         }
     }
 
-    public function getSuppressionListIds($workflow) {
-        $suppressionLists = [];
-
-        foreach ($offerIds as $offerId) {
-            $suppressionLists = array_merge($suppressionLists, $this->offerRepo->getSuppressionListIds($offerId));
-        }
-
-        return array_unique($suppressionLists);
-    }
 }
